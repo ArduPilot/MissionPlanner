8de89ed68098d25cfb5b8ff662783003ecf4623d
Michael Oborne <meee146@gmail.com>
2014-04-23 21:42:50 +0800

    fix pluginloader exception on start when plugin dir doesnt exist

--- Plugin/PluginLoader.cs
+++ Plugin/PluginLoader.cs
@@ -71,7 +71,12 @@ namespace MissionPlanner.Plugin
 
         public static void LoadAll()
         {
-            String[] files = Directory.GetFiles(Application.StartupPath +  Path.DirectorySeparatorChar+ "Plugins" +  Path.DirectorySeparatorChar, "*.dll");
+            string path = Application.StartupPath +  Path.DirectorySeparatorChar+ "Plugins" +  Path.DirectorySeparatorChar;
+
+            if (!Directory.Exists(path))
+                return;
+
+            String[] files = Directory.GetFiles(path, "*.dll");
             foreach (var s in files)
                 Load(Path.Combine(Environment.CurrentDirectory, s));
 
