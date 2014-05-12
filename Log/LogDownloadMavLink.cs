86bd688390c6652839b0d537a462706549b5c210
Michael Oborne <meee146@gmail.com>
2014-05-12 08:01:14 +0800

    add clear logs verify

--- Log/LogDownloadMavLink.cs
+++ Log/LogDownloadMavLink.cs
@@ -304,15 +304,18 @@ namespace MissionPlanner.Log
 
         private void BUT_clearlogs_Click(object sender, EventArgs e)
         {
-            try
+            if (CustomMessageBox.Show("Are you sure?", "sure", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
             {
-                MainV2.comPort.EraseLog();
-                TXT_seriallog.AppendText("!!Allow 30-90 seconds for erase\n");
-                status = serialstatus.Done;
-                updateDisplay();
-                CHK_logs.Items.Clear();
+                try
+                {
+                    MainV2.comPort.EraseLog();
+                    TXT_seriallog.AppendText("!!Allow 30-90 seconds for erase\n");
+                    status = serialstatus.Done;
+                    updateDisplay();
+                    CHK_logs.Items.Clear();
+                }
+                catch (Exception ex) { CustomMessageBox.Show(ex.Message, "Error"); }
             }
-            catch (Exception ex) { CustomMessageBox.Show(ex.Message, "Error"); }
         }
 
         private void BUT_redokml_Click(object sender, EventArgs e)
