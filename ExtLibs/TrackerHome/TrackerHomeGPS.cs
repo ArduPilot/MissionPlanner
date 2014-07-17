8d7fed7ed9535c9af37c8880427b7e7f36a20e76
Michael Oborne <meee146@gmail.com>
2014-07-17 08:09:40 +0800

    update set tracker here to use srtm alt.

--- ExtLibs/TrackerHome/TrackerHomeGPS.cs
+++ ExtLibs/TrackerHome/TrackerHomeGPS.cs
@@ -95,6 +95,8 @@ using TrackerHomeGPS;
         void setFromPlannerLocation(object sender, EventArgs e)
         {
             Host.comPort.MAV.cs.TrackerLocation = new PointLatLngAlt(this.Host.FPMenuMapPosition);
+
+            Host.comPort.MAV.cs.TrackerLocation.Alt = srtm.getAltitude(Host.comPort.MAV.cs.TrackerLocation.Lat, Host.comPort.MAV.cs.TrackerLocation.Lng);
         }
 
         void setTrackerHomeFromModule(object sender, EventArgs e)
