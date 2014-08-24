c9868c4507e0fa44c73690f2a98c109863a6ef4e
Michael Oborne <meee146@gmail.com>
2014-08-25 07:13:36 +0800

    Firmware: update antenna tracker icon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace MissionPlanner.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Antenna_Tracker_01 {
+            get {
+                object obj = ResourceManager.GetObject("Antenna Tracker-01", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap APM_airframes_001 {
             get {
                 object obj = ResourceManager.GetObject("APM_airframes_001", resourceCulture);
