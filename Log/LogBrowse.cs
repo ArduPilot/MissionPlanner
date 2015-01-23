006407561576399ad90b7158fc48cbf4cba2d4d6
Michael Oborne <meee146@gmail.com>
2015-01-24 06:56:04 +0800

    logbrowse: add better cleanup on load/reload #782

--- Log/LogBrowse.cs
+++ Log/LogBrowse.cs
@@ -170,6 +170,15 @@ namespace MissionPlanner.Log
             mapoverlay.Clear();
             markeroverlay.Clear();
 
+            logdatafilter.Clear();
+
+            ErrorCache = new List<TextObj>();
+            ModeCache = new List<TextObj>();
+            TimeCache = new List<TextObj>();
+
+            seenmessagetypes = new Hashtable();
+            m_timeTable = new Dictionary<int, int>();
+
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             openFileDialog1.Filter = "Log Files|*.log;*.bin";
             openFileDialog1.FilterIndex = 2;
