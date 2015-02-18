24938a273daf9f7494e2be5f9fe4609b48b0e9a0
SkiTiSu <sjtsjjy@163.com>
2015-02-18 14:13:38 +0800

    Locallization: update Params zh-Hans translation

--- Controls/ConnectionControl.Designer.cs
+++ Controls/ConnectionControl.Designer.cs
@@ -37,6 +37,7 @@
             // 
             // cmb_Baud
             // 
+            resources.ApplyResources(this.cmb_Baud, "cmb_Baud");
             this.cmb_Baud.BackColor = System.Drawing.Color.Black;
             this.cmb_Baud.DropDownWidth = 150;
             this.cmb_Baud.ForeColor = System.Drawing.Color.White;
@@ -54,25 +55,24 @@
             resources.GetString("cmb_Baud.Items9"),
             resources.GetString("cmb_Baud.Items10"),
             resources.GetString("cmb_Baud.Items11")});
-            resources.ApplyResources(this.cmb_Baud, "cmb_Baud");
             this.cmb_Baud.Name = "cmb_Baud";
             // 
             // cmb_ConnectionType
             // 
+            resources.ApplyResources(this.cmb_ConnectionType, "cmb_ConnectionType");
             this.cmb_ConnectionType.BackColor = System.Drawing.Color.Black;
             this.cmb_ConnectionType.ForeColor = System.Drawing.Color.White;
             this.cmb_ConnectionType.FormattingEnabled = true;
-            resources.ApplyResources(this.cmb_ConnectionType, "cmb_ConnectionType");
             this.cmb_ConnectionType.Name = "cmb_ConnectionType";
             // 
             // cmb_Connection
             // 
+            resources.ApplyResources(this.cmb_Connection, "cmb_Connection");
             this.cmb_Connection.BackColor = System.Drawing.Color.Black;
             this.cmb_Connection.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.cmb_Connection.DropDownWidth = 200;
             this.cmb_Connection.ForeColor = System.Drawing.Color.White;
             this.cmb_Connection.FormattingEnabled = true;
-            resources.ApplyResources(this.cmb_Connection, "cmb_Connection");
             this.cmb_Connection.Name = "cmb_Connection";
             this.cmb_Connection.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.cmb_Connection_DrawItem);
             // 
--- Controls/ConnectionStats.Designer.cs
+++ Controls/ConnectionStats.Designer.cs
@@ -31,6 +31,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConnectionStats));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -57,150 +58,103 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 18);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Bytes";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(135, 18);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Bytes/s";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 44);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Packets";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 70);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Pkts Lost";
             // 
             // txt_BytesReceived
             // 
+            resources.ApplyResources(this.txt_BytesReceived, "txt_BytesReceived");
             this.txt_BytesReceived.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_BytesReceived.Location = new System.Drawing.Point(58, 16);
             this.txt_BytesReceived.Name = "txt_BytesReceived";
             this.txt_BytesReceived.ReadOnly = true;
-            this.txt_BytesReceived.Size = new System.Drawing.Size(64, 20);
-            this.txt_BytesReceived.TabIndex = 4;
             // 
             // txt_BytesPerSecondRx
             // 
+            resources.ApplyResources(this.txt_BytesPerSecondRx, "txt_BytesPerSecondRx");
             this.txt_BytesPerSecondRx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_BytesPerSecondRx.Location = new System.Drawing.Point(184, 16);
             this.txt_BytesPerSecondRx.Name = "txt_BytesPerSecondRx";
             this.txt_BytesPerSecondRx.ReadOnly = true;
-            this.txt_BytesPerSecondRx.Size = new System.Drawing.Size(51, 20);
-            this.txt_BytesPerSecondRx.TabIndex = 5;
             // 
             // txt_PacketsRx
             // 
+            resources.ApplyResources(this.txt_PacketsRx, "txt_PacketsRx");
             this.txt_PacketsRx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_PacketsRx.Location = new System.Drawing.Point(58, 42);
             this.txt_PacketsRx.Name = "txt_PacketsRx";
             this.txt_PacketsRx.ReadOnly = true;
-            this.txt_PacketsRx.Size = new System.Drawing.Size(64, 20);
-            this.txt_PacketsRx.TabIndex = 6;
             // 
             // txt_PacketsLost
             // 
+            resources.ApplyResources(this.txt_PacketsLost, "txt_PacketsLost");
             this.txt_PacketsLost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_PacketsLost.Location = new System.Drawing.Point(58, 68);
             this.txt_PacketsLost.Name = "txt_PacketsLost";
             this.txt_PacketsLost.ReadOnly = true;
-            this.txt_PacketsLost.Size = new System.Drawing.Size(64, 20);
-            this.txt_PacketsLost.TabIndex = 7;
             // 
             // txt_LinkQuality
             // 
+            resources.ApplyResources(this.txt_LinkQuality, "txt_LinkQuality");
             this.txt_LinkQuality.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_LinkQuality.Location = new System.Drawing.Point(184, 68);
             this.txt_LinkQuality.Name = "txt_LinkQuality";
             this.txt_LinkQuality.ReadOnly = true;
-            this.txt_LinkQuality.Size = new System.Drawing.Size(51, 20);
-            this.txt_LinkQuality.TabIndex = 9;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(139, 68);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(39, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Quality";
             // 
             // txt_PacketsPerSecond
             // 
+            resources.ApplyResources(this.txt_PacketsPerSecond, "txt_PacketsPerSecond");
             this.txt_PacketsPerSecond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_PacketsPerSecond.Location = new System.Drawing.Point(184, 42);
             this.txt_PacketsPerSecond.Name = "txt_PacketsPerSecond";
             this.txt_PacketsPerSecond.ReadOnly = true;
-            this.txt_PacketsPerSecond.Size = new System.Drawing.Size(51, 20);
-            this.txt_PacketsPerSecond.TabIndex = 10;
             // 
             // txt_BytesSent
             // 
+            resources.ApplyResources(this.txt_BytesSent, "txt_BytesSent");
             this.txt_BytesSent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_BytesSent.Location = new System.Drawing.Point(74, 16);
             this.txt_BytesSent.Name = "txt_BytesSent";
             this.txt_BytesSent.ReadOnly = true;
-            this.txt_BytesSent.Size = new System.Drawing.Size(64, 20);
-            this.txt_BytesSent.TabIndex = 12;
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 18);
+            resources.ApplyResources(this.label6, "label6");
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 13);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Total Bytes";
             // 
             // label7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(23, 43);
+            resources.ApplyResources(this.label7, "label7");
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(43, 13);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Bytes/s";
             // 
             // txt_BytesPerSecondSent
             // 
+            resources.ApplyResources(this.txt_BytesPerSecondSent, "txt_BytesPerSecondSent");
             this.txt_BytesPerSecondSent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_BytesPerSecondSent.Location = new System.Drawing.Point(74, 42);
             this.txt_BytesPerSecondSent.Name = "txt_BytesPerSecondSent";
             this.txt_BytesPerSecondSent.ReadOnly = true;
-            this.txt_BytesPerSecondSent.Size = new System.Drawing.Size(64, 20);
-            this.txt_BytesPerSecondSent.TabIndex = 14;
             // 
             // label8
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(126, 43);
+            resources.ApplyResources(this.label8, "label8");
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(56, 13);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "Packets/s";
             // 
             // groupBox1
             // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.label10);
             this.groupBox1.Controls.Add(this.txt_MaxPacketInterval);
             this.groupBox1.Controls.Add(this.label8);
@@ -215,51 +169,38 @@
             this.groupBox1.Controls.Add(this.txt_PacketsRx);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.txt_PacketsLost);
-            this.groupBox1.Location = new System.Drawing.Point(4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(245, 124);
-            this.groupBox1.TabIndex = 16;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Download";
             // 
             // label10
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(13, 96);
+            resources.ApplyResources(this.label10, "label10");
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(156, 13);
-            this.label10.TabIndex = 16;
-            this.label10.Text = "Max time between packets (ms)";
             // 
             // txt_MaxPacketInterval
             // 
+            resources.ApplyResources(this.txt_MaxPacketInterval, "txt_MaxPacketInterval");
             this.txt_MaxPacketInterval.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_MaxPacketInterval.Location = new System.Drawing.Point(184, 94);
             this.txt_MaxPacketInterval.Name = "txt_MaxPacketInterval";
             this.txt_MaxPacketInterval.ReadOnly = true;
-            this.txt_MaxPacketInterval.Size = new System.Drawing.Size(51, 20);
-            this.txt_MaxPacketInterval.TabIndex = 17;
             // 
             // groupBox2
             // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Controls.Add(this.txt_BytesPerSecondSent);
             this.groupBox2.Controls.Add(this.label7);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.txt_BytesSent);
-            this.groupBox2.Location = new System.Drawing.Point(255, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(144, 83);
-            this.groupBox2.TabIndex = 17;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Upload";
             // 
             // ConnectionStats
             // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Name = "ConnectionStats";
-            this.Size = new System.Drawing.Size(408, 136);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
