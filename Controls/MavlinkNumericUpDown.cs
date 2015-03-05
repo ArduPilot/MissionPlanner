7fab7ed84d8aa9c2e4f79d26de017bc27ae85812
Michael Oborne <meee146@gmail.com>
2015-03-05 22:42:57 +0800

    mavlinknumericupdown: add post value update, auto min,max, and multiple paramnames

--- Controls/MavlinkNumericUpDown.cs
+++ Controls/MavlinkNumericUpDown.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Collections;
+using MissionPlanner.Utilities;
 
 namespace MissionPlanner.Controls
 {
@@ -24,6 +25,20 @@ namespace MissionPlanner.Controls
         Control _control;
         float _scale = 1;
 
+        public class ParamChanged : EventArgs
+        {
+            public string name;
+            public float value;
+
+            public ParamChanged(string Name, float Value)
+            {
+                this.name = Name;
+                this.value = Value;
+            }
+        }
+
+        [System.ComponentModel.Browsable(true)]
+        public event EventHandler ValueUpdated;
 
         public MavlinkNumericUpDown()
         {
@@ -37,25 +52,53 @@ namespace MissionPlanner.Controls
 
         public void setup(float Min, float Max, float Scale, float Increment, string paramname, Hashtable paramlist, Control enabledisable = null)
         {
+            setup(Min, Max, Scale, Increment, new string[] { paramname }, paramlist, enabledisable);
+        }
+
+        public void setup(float Min, float Max, float Scale, float Increment, string[] paramname, Hashtable paramlist, Control enabledisable = null)
+        {
             this.ValueChanged -= MavlinkNumericUpDown_ValueChanged;
+
+            // default to first item
+            this.ParamName = paramname[0];
+            // set a new item is first item doesnt exist
+            foreach (var paramn in paramname)
+            {
+                if (paramlist.ContainsKey(paramn))
+                {
+                    this.ParamName = paramn;
+                    break;
+                }
+            }
+
+            // update local name
+            Name = ParamName;
+            // set min and max of both are equal
+            if (Min == Max)
+            {
+                double mint = Min, maxt = Max;
+                ParameterMetaDataRepository.GetParameterRange(ParamName, ref mint, ref maxt, MainV2.comPort.MAV.cs.firmware.ToString());
+                Min = (float)mint;
+                Max = (float)maxt;
+            }
 
             _scale = Scale;
             this.Minimum = (decimal)(Min);
             this.Maximum = (decimal)(Max);
             this.Increment = (decimal)(Increment);
             this.DecimalPlaces = BitConverter.GetBytes(decimal.GetBits((decimal)Increment)[3])[2];
-            this.ParamName = paramname;
+  
             this.param = paramlist;
             this._control = enabledisable;
 
-            if (paramlist.ContainsKey(paramname))
+            if (paramlist.ContainsKey(ParamName))
             {
                 this.Enabled = true;
                 this.Visible = true;
 
                 enableControl(true);
 
-                decimal value = (decimal)((float)paramlist[paramname] / _scale);
+                decimal value = (decimal)((float)paramlist[ParamName] / _scale);
 
                 int dec = BitConverter.GetBytes(decimal.GetBits((decimal)value)[3])[2];
 
@@ -67,7 +110,7 @@ namespace MissionPlanner.Controls
                 if (value > this.Maximum)
                     this.Maximum = value;
 
-                this.Value = value;
+                base.Value = value;
 
             }
             else
@@ -90,9 +133,16 @@ namespace MissionPlanner.Controls
 
         void MavlinkNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
+            if (ValueUpdated!= null)
+            {
+                this.UpdateEditText();
+                ValueUpdated(this, new ParamChanged(ParamName, (float)base.Value * (float)_scale));
+                return;
+            }
+
             try
             {
-                bool ans = MainV2.comPort.setParam(ParamName, (float)this.Value * _scale);
+                bool ans = MainV2.comPort.setParam(ParamName, (float)base.Value * (float)_scale);
                 if (ans == false)
                     CustomMessageBox.Show(String.Format(Strings.ErrorSetValueFailed,ParamName), Strings.ERROR);
             }
