f4f9c54ddb06bcf670af9a3bcbe3788fd6d58761
Michael Oborne <meee146@gmail.com>
2015-04-03 03:01:39 +0800

    GimbalPoint: update code

--- Utilities/GimbalPoint.cs
+++ Utilities/GimbalPoint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 
@@ -48,7 +49,7 @@ namespace MissionPlanner.Utilities
 
         public static double ConvertPwmtoAngle(axis axis)
         {
-            int pwmvalue =-1;
+            int pwmvalue = -1;
 
             if (!MainV2.comPort.MAV.param.ContainsKey("RC" + yawchannel + "_MIN"))
                 return 0;
@@ -63,8 +64,8 @@ namespace MissionPlanner.Utilities
                     float maxroll = (float)MainV2.comPort.MAV.param["MNT_ANGMAX_ROL"];
                     float revr = (float)MainV2.comPort.MAV.param["RC" + rollchannel + "_REV"];
 
-                    return angle_input(revr != 1,pwmvalue,minr,maxr,minroll,maxroll)/100.0;
-                    
+                    return angle_input(revr != 1, pwmvalue, minr, maxr, minroll, maxroll) / 100.0;
+
                 case GimbalPoint.axis.pitch:
                     pwmvalue = channelpwm(pitchchannel);
                     float minp = (float)MainV2.comPort.MAV.param["RC" + pitchchannel + "_MIN"];
@@ -75,7 +76,7 @@ namespace MissionPlanner.Utilities
 
 
                     return angle_input(revp != 1, pwmvalue, minp, maxp, minpitch, maxpitch) / 100.0;
-                    
+
                 case GimbalPoint.axis.yaw:
                     pwmvalue = channelpwm(yawchannel);
                     float miny = (float)MainV2.comPort.MAV.param["RC" + yawchannel + "_MIN"];
@@ -84,12 +85,29 @@ namespace MissionPlanner.Utilities
                     float maxyaw = (float)MainV2.comPort.MAV.param["MNT_ANGMAX_PAN"];
                     float revy = (float)MainV2.comPort.MAV.param["RC" + yawchannel + "_REV"];
 
-
                     return angle_input(revy != 1, pwmvalue, miny, maxy, minyaw, maxyaw) / 100.0;
-                    
             }
 
             return 0;
+        }
+
+        public static PointF FindLineIntersection(PointF start1, PointF end1, PointF start2, PointF end2)
+        {
+            double denom = ((end1.X - start1.X) * (end2.Y - start2.Y)) - ((end1.Y - start1.Y) * (end2.X - start2.X));
+            //  AB & CD are parallel         
+            if (denom == 0)
+                return new PointF();
+            double numer = ((start1.Y - start2.Y) * (end2.X - start2.X)) - ((start1.X - start2.X) * (end2.Y - start2.Y));
+            double r = numer / denom;
+            double numer2 = ((start1.Y - start2.Y) * (end1.X - start1.X)) - ((start1.X - start2.X) * (end1.Y - start1.Y));
+            double s = numer2 / denom;
+            if ((r < 0 || r > 1) || (s < 0 || s > 1))
+                return new PointF();
+            // Find intersection point      
+            PointF result = new PointF();
+            result.X = (float)(start1.X + (r * (end1.X - start1.X)));
+            result.Y = (float)(start1.Y + (r * (end1.Y - start1.Y)));
+            return result;
         }
 
         public const double rad2deg = (180 / System.Math.PI);
@@ -115,7 +133,6 @@ namespace MissionPlanner.Utilities
             double rollangle = MainV2.comPort.MAV.cs.campointb;
             double pitchangle = MainV2.comPort.MAV.cs.campointa;
 
-
             if (Math.Abs(rollangle) > 180 || yawangle == 0 && pitchangle == 0)
             {
                 yawangle = ConvertPwmtoAngle(axis.yaw);
@@ -127,14 +144,41 @@ namespace MissionPlanner.Utilities
 
             // tan (o/a)
             // todo account for ground elevation change.
-            double dist = Math.Tan((90 +pitchangle)* deg2rad) * (MainV2.comPort.MAV.cs.alt);
 
-            if (dist > 9999)
-                return PointLatLngAlt.Zero;
+            int distout = 10;
+            PointLatLngAlt newpos = PointLatLngAlt.Zero;
+
+            //dist = Math.Tan((90 + pitchangle) * deg2rad) * (MainV2.comPort.MAV.cs.alt);
+
+            while (distout < 1000)
+            {
+                // get a projected point to test intersection against
+                PointLatLngAlt newposdist = currentlocation.newpos(yawangle + MainV2.comPort.MAV.cs.yaw, distout);
+                newposdist.Alt = srtm.getAltitude(newposdist.Lat, newposdist.Lng).alt;
+
+                // get another point
+                PointLatLngAlt newposdist2 = currentlocation.newpos(yawangle + MainV2.comPort.MAV.cs.yaw, distout + 50);
+                newposdist2.Alt = srtm.getAltitude(newposdist2.Lat, newposdist2.Lng).alt;
+
+                // get the flat terrain distance out - at 0 alt
+                double distflat = Math.Tan((90 + pitchangle) * deg2rad) * (MainV2.comPort.MAV.cs.altasl);
+
+                // x is dist from plane, y is alt
+                var newpoint = FindLineIntersection(new PointF(0,MainV2.comPort.MAV.cs.altasl), new PointF((float)distflat, 0),
+                    new PointF((float)distout, (float)newposdist.Alt), new PointF((float)distout + 50, (float)newposdist2.Alt));
+
+                if (newpoint.X != 0)
+                {
+                    newpos = newposdist2;
+                    break;
+                }
+
+                distout += 50;
+            }
 
             //Console.WriteLine("pitch " + pitchangle.ToString("0.000") + " yaw " + yawangle.ToString("0.000") + " dist" + dist.ToString("0.000"));
 
-            PointLatLngAlt newpos = currentlocation.newpos( yawangle + MainV2.comPort.MAV.cs.yaw, dist);
+            //PointLatLngAlt newpos = currentlocation.newpos( yawangle + MainV2.comPort.MAV.cs.yaw, dist);
 
             //Console.WriteLine(newpos);
             return newpos;
