cbfcd7ba183419b5a3c68b125d6de353ee6c5346
Michael Oborne <meee146@gmail.com>
2015-05-03 16:21:53 +0800

    CommsFile: add read exception when not connected

--- ExtLibs/Comms/CommsFile.cs
+++ ExtLibs/Comms/CommsFile.cs
@@ -36,6 +36,9 @@ namespace MissionPlanner.Comms
         }
         public int Read(byte[] buffer, int offset, int count)
         {
+            if (!IsOpen)
+                throw new EndOfStreamException("File not open");
+
             while (true) 
             {
                 // check if we have credit and continue
