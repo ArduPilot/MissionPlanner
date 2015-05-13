5858f735d3ceae29c96c6c197b04d5e13cf51b5d
Michael Oborne <meee146@gmail.com>
2015-05-14 07:07:24 +0800

    Script: update example 2

--- Scripts/example2.py
+++ Scripts/example2.py
@@ -2,6 +2,9 @@
 import clr
 import MissionPlanner
 clr.AddReference("MAVLink")
+from System import Byte
+import MAVLink
+from MAVLink import mavlink_command_long_t
 
 import MAVLink
 
@@ -9,9 +12,11 @@ print 'Start Script'
 
 MAV.doCommand(MAVLink.MAV_CMD.DO_DIGICAM_CONTROL, 0, 0, 0, 0, 0, 0, 0);
 
-MAV.doARM(true);
+MAV.doCommand(MAVLink.MAV_CMD.DO_DIGICAM_CONTROL, MAVLink.MAV_MOUNT_MODE.NEUTRAL.value__, 0, 0, 0, 0, 0, 0);
 
-MAV.doReboot(false);
+MAV.doARM(True);
+
+#MAV.doReboot(False);
 
 MAV.setWPCurrent(1);
 
@@ -24,3 +29,11 @@ MAV.getWPCount();
 MAV.getWP(1);
 
 MAV.getParamList();
+
+commandlong = mavlink_command_long_t()
+mavlink_command_long_t.target_system.SetValue(commandlong,71)
+mavlink_command_long_t.target_component.SetValue(commandlong,67)
+mavlink_command_long_t.param1.SetValue(commandlong, MAVLink.MAV_MOUNT_MODE.NEUTRAL.value__)
+mavlink_command_long_t.command.SetValue(commandlong, MAVLink.MAV_CMD.DO_DIGICAM_CONTROL.value__)
+
+MAV.sendPacket(commandlong)
