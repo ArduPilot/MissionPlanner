14da93cd4623bb348eb469661ce70a912d5310fd
四季天书SkiTiSu <sjtsjjy@163.com>
2015-05-28 21:21:37 +0800

    Strings: add AutoWP

--- ExtLibs/MissionPlanner.Gridv2/GridPluginv2.cs
+++ ExtLibs/MissionPlanner.Gridv2/GridPluginv2.cs
@@ -46,7 +46,7 @@ namespace MissionPlanner
             int index = col.Count;
             foreach (ToolStripItem item in col)
             {
-                if (item.Text.Equals("Auto WP"))
+                if (item.Text.Equals(Strings.AutoWP))
                 {
                     index = col.IndexOf(item);
                     ((ToolStripMenuItem)item).DropDownItems.Add(but);
