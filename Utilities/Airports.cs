178cb18d21da4570a5fd24c090b1587a5ce4c861
Michael Oborne <meee146@gmail.com>
2015-05-30 20:10:02 +0800

    Aitports: add seaplane base filter

--- Utilities/Airports.cs
+++ Utilities/Airports.cs
@@ -164,7 +164,7 @@ namespace MissionPlanner.Utilities
                     if (items[1].Length != 6) // "xxxx"
                         continue;
 
-                    if (items[2].Contains("small_airport") || items[2].Contains("heliport") || items[2].Contains("closed"))
+                    if (items[2].Contains("small_airport") || items[2].Contains("seaplane_base") || items[2].Contains("heliport") || items[2].Contains("closed"))
                         continue;
 
 
