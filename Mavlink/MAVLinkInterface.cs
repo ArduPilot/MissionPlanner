638896fceeca294d7c2fd51e2b872d0312a63d09
DroneBuster <matas.razgunas@gmail.com>
2015-11-23 18:19:58 +0200

    Make waypoints appear on flight data screen on WP read

--- Mavlink/MAVLinkInterface.cs
+++ Mavlink/MAVLinkInterface.cs
@@ -1796,6 +1796,8 @@ Please check the following
                         log.InfoFormat("getWP {0} {1} {2} {3} {4} opt {5}", loc.id, loc.p1, loc.alt, loc.lat, loc.lng,
                             loc.options);
 
+                        MAV.wps[wp.seq] = wp; 
+
                         break;
                     }
                     else
