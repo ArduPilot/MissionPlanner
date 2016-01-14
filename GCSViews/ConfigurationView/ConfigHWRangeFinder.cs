f84eedcd9d883e8108af197391bf3c0c3788ac39
Michael Oborne <mich146@hotmail.com>
2016-01-15 06:26:19 +0800

    ConfigHWRangeFinder: add TeraRanger One support

--- GCSViews/ConfigurationView/ConfigHWRangeFinder.cs
+++ GCSViews/ConfigurationView/ConfigHWRangeFinder.cs
@@ -7,6 +7,7 @@ namespace MissionPlanner.GCSViews.ConfigurationView
 {
     public partial class ConfigHWRangeFinder : UserControl, IActivate, IDeactivate
     {
+        bool startup = true;
         private const float rad2deg = (float) (180/Math.PI);
         private const float deg2rad = (float) (1.0/rad2deg);
 
@@ -23,12 +24,15 @@ namespace MissionPlanner.GCSViews.ConfigurationView
                 return;
             }
             Enabled = true;
+            startup = true;
 
             CMB_sonartype.setup(
                 ParameterMetaDataRepository.GetParameterOptionsInt("RNGFND_TYPE",
                     MainV2.comPort.MAV.cs.firmware.ToString()), "RNGFND_TYPE", MainV2.comPort.MAV.param);
 
             timer1.Start();
+
+            startup = false;
         }
 
         public void Deactivate()
@@ -40,6 +44,19 @@ namespace MissionPlanner.GCSViews.ConfigurationView
         {
             LBL_dist.Text = MainV2.comPort.MAV.cs.sonarrange.ToString();
             LBL_volt.Text = MainV2.comPort.MAV.cs.sonarvoltage.ToString();
+        }
+
+        private void CMB_sonartype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (startup)
+                return;
+
+            if (CMB_sonartype.Text == "TeraRangerOne-I2C")
+            {
+                // set min and max to 20cm - 10m
+                MainV2.comPort.setParam("RNGFND_MAX_CM", 100);
+                MainV2.comPort.setParam("RNGFND_MIN_CM ", 20);
+            }
         }
     }
 }
