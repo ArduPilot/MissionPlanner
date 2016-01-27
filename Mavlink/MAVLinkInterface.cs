1f3f12e6b5bd7464c6ce621cea39a94fbbe41134
Tom Pittenger <magicrub@gmail.com>
2016-01-26 20:34:21 -0800

    Mavlink: add abort landing method

--- Mavlink/MAVLinkInterface.cs
+++ Mavlink/MAVLinkInterface.cs
@@ -1341,6 +1341,11 @@ Please check the following
             return doCommand(MAV_CMD.COMPONENT_ARM_DISARM, armit ? 1 : 0, 21196, 0, 0, 0, 0, 0);
         }
 
+        public bool doAbortLand()
+        {
+            return doCommand(MAV_CMD.DO_GO_AROUND, 0, 0, 0, 0, 0, 0, 0);
+        }
+
         public bool doMotorTest(int motor, MAVLink.MOTOR_TEST_THROTTLE_TYPE thr_type, int throttle, int timeout)
         {
             return MainV2.comPort.doCommand(MAVLink.MAV_CMD.DO_MOTOR_TEST, (float) motor, (float) (byte) thr_type,
