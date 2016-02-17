f56d2ef8e87179491746eba60f2a18195a76c3f4
四季天书SkiTiSu <sjtsjjy@gmail.com>
2016-02-17 23:16:24 +0800

    L10N: update zh-Hans help text

--- L10N.cs
+++ L10N.cs
@@ -93,17 +93,24 @@ namespace MissionPlanner
 
         public static bool Ping(string ip)
         {
-            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
-            System.Net.NetworkInformation.PingOptions options = new System.Net.NetworkInformation.PingOptions();
-            options.DontFragment = true;
-            string data = "MissionPlanner";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            int timeout = 500;
-            System.Net.NetworkInformation.PingReply reply = p.Send(ip, timeout, buffer, options);
-            if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
-                return true;
-            else
+            try
+            {
+                System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+                System.Net.NetworkInformation.PingOptions options = new System.Net.NetworkInformation.PingOptions();
+                options.DontFragment = true;
+                string data = "MissionPlanner";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 500;
+                System.Net.NetworkInformation.PingReply reply = p.Send(ip, timeout, buffer, options);
+                if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+                    return true;
+                else
+                    return false;
+            }
+            catch
+            {
                 return false;
+            }
         }
 
         public static bool CheckHTTP(string url)
