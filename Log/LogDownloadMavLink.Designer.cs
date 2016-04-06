b1919c6dbbd5bb3d8c62cd81bb62822b60f1cea5
Chris Lovett <clovett@microsoft.com>
2016-04-05 19:02:53 -0700

    Fix bugs in LogDownloadMavLink dialog.  Dispose SerialPort Streams properly.  Fix null reference in MainV2 form.

--- Log/LogDownloadMavLink.Designer.cs
+++ Log/LogDownloadMavLink.Designer.cs
@@ -28,23 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogDownloadMavLink));
             this.TXT_seriallog = new System.Windows.Forms.TextBox();
             this.BUT_DLall = new MissionPlanner.Controls.MyButton();
             this.BUT_DLthese = new MissionPlanner.Controls.MyButton();
             this.BUT_clearlogs = new MissionPlanner.Controls.MyButton();
             this.CHK_logs = new System.Windows.Forms.CheckedListBox();
-            this.TXT_status = new System.Windows.Forms.TextBox();
             this.BUT_redokml = new MissionPlanner.Controls.MyButton();
             this.BUT_firstperson = new MissionPlanner.Controls.MyButton();
             this.BUT_bintolog = new MissionPlanner.Controls.MyButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LabelStatus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelBytes = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // TXT_seriallog
             // 
             resources.ApplyResources(this.TXT_seriallog, "TXT_seriallog");
             this.TXT_seriallog.Name = "TXT_seriallog";
-            this.TXT_seriallog.ReadOnly = true;
+            this.tableLayoutPanel1.SetRowSpan(this.TXT_seriallog, 2);
             // 
             // BUT_DLall
             // 
@@ -57,6 +69,7 @@
             // 
             resources.ApplyResources(this.BUT_DLthese, "BUT_DLthese");
             this.BUT_DLthese.Name = "BUT_DLthese";
+            this.toolTip1.SetToolTip(this.BUT_DLthese, resources.GetString("BUT_DLthese.ToolTip"));
             this.BUT_DLthese.UseVisualStyleBackColor = true;
             this.BUT_DLthese.Click += new System.EventHandler(this.BUT_DLthese_Click);
             // 
@@ -64,27 +77,22 @@
             // 
             resources.ApplyResources(this.BUT_clearlogs, "BUT_clearlogs");
             this.BUT_clearlogs.Name = "BUT_clearlogs";
+            this.toolTip1.SetToolTip(this.BUT_clearlogs, resources.GetString("BUT_clearlogs.ToolTip"));
             this.BUT_clearlogs.UseVisualStyleBackColor = true;
             this.BUT_clearlogs.Click += new System.EventHandler(this.BUT_clearlogs_Click);
             // 
             // CHK_logs
             // 
+            resources.ApplyResources(this.CHK_logs, "CHK_logs");
             this.CHK_logs.CheckOnClick = true;
             this.CHK_logs.FormattingEnabled = true;
-            resources.ApplyResources(this.CHK_logs, "CHK_logs");
             this.CHK_logs.Name = "CHK_logs";
-            // 
-            // TXT_status
-            // 
-            resources.ApplyResources(this.TXT_status, "TXT_status");
-            this.TXT_status.ForeColor = System.Drawing.Color.Red;
-            this.TXT_status.Name = "TXT_status";
-            this.TXT_status.ReadOnly = true;
             // 
             // BUT_redokml
             // 
             resources.ApplyResources(this.BUT_redokml, "BUT_redokml");
             this.BUT_redokml.Name = "BUT_redokml";
+            this.toolTip1.SetToolTip(this.BUT_redokml, resources.GetString("BUT_redokml.ToolTip"));
             this.BUT_redokml.UseVisualStyleBackColor = true;
             this.BUT_redokml.Click += new System.EventHandler(this.BUT_redokml_Click);
             // 
@@ -92,6 +100,7 @@
             // 
             resources.ApplyResources(this.BUT_firstperson, "BUT_firstperson");
             this.BUT_firstperson.Name = "BUT_firstperson";
+            this.toolTip1.SetToolTip(this.BUT_firstperson, resources.GetString("BUT_firstperson.ToolTip"));
             this.BUT_firstperson.UseVisualStyleBackColor = true;
             this.BUT_firstperson.Click += new System.EventHandler(this.BUT_firstperson_Click);
             // 
@@ -99,24 +108,84 @@
             // 
             resources.ApplyResources(this.BUT_bintolog, "BUT_bintolog");
             this.BUT_bintolog.Name = "BUT_bintolog";
+            this.toolTip1.SetToolTip(this.BUT_bintolog, resources.GetString("BUT_bintolog.ToolTip"));
             this.BUT_bintolog.UseVisualStyleBackColor = true;
             this.BUT_bintolog.Click += new System.EventHandler(this.BUT_bintolog_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.TXT_seriallog, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.CHK_logs, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.LabelStatus, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // LabelStatus
+            // 
+            resources.ApplyResources(this.LabelStatus, "LabelStatus");
+            this.LabelStatus.ForeColor = System.Drawing.Color.SeaGreen;
+            this.LabelStatus.Name = "LabelStatus";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // tableLayoutPanel2
+            // 
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
+            this.tableLayoutPanel2.Controls.Add(this.progressBar1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.labelBytes, 1, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // labelBytes
+            // 
+            resources.ApplyResources(this.labelBytes, "labelBytes");
+            this.labelBytes.Name = "labelBytes";
+            // 
+            // tableLayoutPanel3
+            // 
+            resources.ApplyResources(this.tableLayoutPanel3, "tableLayoutPanel3");
+            this.tableLayoutPanel3.Controls.Add(this.BUT_DLall, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.BUT_clearlogs, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.BUT_firstperson, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.BUT_DLthese, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.BUT_bintolog, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this.BUT_redokml, 1, 1);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 400;
+            this.toolTip1.ShowAlways = true;
             // 
             // LogDownloadMavLink
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.BUT_bintolog);
-            this.Controls.Add(this.BUT_firstperson);
-            this.Controls.Add(this.BUT_redokml);
-            this.Controls.Add(this.TXT_status);
-            this.Controls.Add(this.CHK_logs);
-            this.Controls.Add(this.BUT_clearlogs);
-            this.Controls.Add(this.BUT_DLthese);
-            this.Controls.Add(this.BUT_DLall);
-            this.Controls.Add(this.TXT_seriallog);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "LogDownloadMavLink";
             this.Load += new System.EventHandler(this.Log_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,10 +197,18 @@
         private Controls.MyButton BUT_DLthese;
         private Controls.MyButton BUT_clearlogs;
         private System.Windows.Forms.CheckedListBox CHK_logs;
-        private System.Windows.Forms.TextBox TXT_status;
         private Controls.MyButton BUT_redokml;
         private System.Windows.Forms.TextBox TXT_seriallog;
         private Controls.MyButton BUT_firstperson;
         private Controls.MyButton BUT_bintolog;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LabelStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelBytes;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
