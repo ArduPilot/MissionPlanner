3113f70ec6fecbe339b423fd77b2a3d3d24786c7
Michael Oborne <mich146@hotmail.com>
2016-06-26 07:54:07 +0800

    Python: add example 8 speech

---
+++ Scripts/example8 - speech.py
@@ -0,0 +1,19 @@
+﻿import sys
+import math
+import clr
+import time
+clr.AddReference("MissionPlanner")
+import MissionPlanner
+clr.AddReference("MissionPlanner.Utilities") # includes the Utilities class
+
+print 'Start Script'
+
+MissionPlanner.MainV2.speechEnable = True
+
+while True:
+	print 'speech ...'
+	MissionPlanner.MainV2.speechEngine.SpeakAsync("test " + cs.roll.ToString())
+	time.sleep(1)
+
+
+
