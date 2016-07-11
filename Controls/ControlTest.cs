2aff5cfa539c2daa33e678b3dbdc93963f7e9e36
Michael Oborne <mich146@hotmail.com>
2016-07-11 11:40:07 +0800

    ControlTest: add tester

---
+++ Controls/ControlTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MissionPlanner.Controls
+{
+    public partial class ControlTest : Form
+    {
+        Control _ctl;
+
+        public ControlTest(Control ctl)
+        {
+            InitializeComponent();
+
+            _ctl = ctl;
+            _ctl.Dock = DockStyle.Fill;
+            Controls.Add(_ctl);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _ctl.Refresh();
+        }
+    }
+}
