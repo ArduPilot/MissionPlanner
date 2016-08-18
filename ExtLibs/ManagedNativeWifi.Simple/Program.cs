5c7e1da94edb23610bbe4770da94fa7aaecb01f4
Francisco Ferreira <francisco-ferreira@mail.telepac.pt>
2016-08-19 00:50:23 +0100

    Add ManagedNativeWifi Simple external library
    https://github.com/emoacht/ManagedNativeWifi

---
+++ ExtLibs/ManagedNativeWifi.Simple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedNativeWifi.Simple
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (!Debugger.IsAttached)
+				Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+			Debug.WriteLine("[Available Network SSIDs]");
+			NativeWifi.GetAvailableNetworkSsids().ToArray();
+
+			Debug.WriteLine("[Connected Network SSIDs]");
+			NativeWifi.GetConnectedNetworkSsids().ToArray();
+		}
+	}
+}
