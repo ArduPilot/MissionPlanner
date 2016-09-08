c56d88757044c110719b18df7689ce6567f8b950
Michael Oborne <mich146@hotmail.com>
2016-09-09 07:18:48 +0800

    LogAnalyzer: update

--- LogAnalyzer/py2exe/createexe.bat
+++ LogAnalyzer/py2exe/createexe.bat
@@ -1,3 +1,5 @@
+C:\Python27x86\python -m pip install numpy -I
+
 C:\Python27x86\python setup.py py2exe
 
 cd dist
@@ -5,6 +7,8 @@ cd dist
 "C:\Program Files\7-Zip\7z.exe" a -tzip ..\..\LogAnalyzer.zip *
 
 cd ..
+
+C:\Python27\python -m pip install numpy -I
 
 C:\Python27\python setup.py py2exe
 
