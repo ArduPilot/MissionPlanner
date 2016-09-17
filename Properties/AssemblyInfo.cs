15149e182b806fb224a6555f14cd380fa386e652
Michael Oborne <mich146@hotmail.com>
2016-09-17 10:56:02 +0800

    Mission Planner 1.3.40

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -38,7 +38,7 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.3.39")]
+[assembly: AssemblyFileVersion("1.3.40")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
