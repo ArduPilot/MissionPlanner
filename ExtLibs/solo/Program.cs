9cfdb663a78e27dde60fddf9445b51eb1c531168
Michael Oborne <mich146@hotmail.com>
2016-09-18 21:54:29 +0800

    solo: add initial library

---
+++ ExtLibs/solo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace solo
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Solo.getFirmwareUrl();
+
+            Solo.flash_px4(@"C:\temp\ArduCopter-v2.px4");
+
+            //while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
