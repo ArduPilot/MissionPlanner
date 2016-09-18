082b30babeac382fb5595cab56b86ccc17715e62
Michael Oborne <mich146@hotmail.com>
2016-09-18 21:52:59 +0800

    ConfigRawParam: add case with both are enabled

--- GCSViews/ConfigurationView/ConfigRawParams.cs
+++ GCSViews/ConfigurationView/ConfigRawParams.cs
@@ -219,7 +219,9 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             temp.Sort((a,b)=>
             {
                 if (a == null || b == null) return 0;
-                if (a.EndsWith("ENABLE")) return 1; return -1;
+                if (a.EndsWith("ENABLE")) return 1;
+                if (a.EndsWith("ENABLE") && b.EndsWith("ENABLE")) return 0;
+                return -1;
             });
 
             foreach (string value in temp)
