dc4b7e5166d6764ef28cf009bbe9d92c772a7391
Michael Oborne <mich146@hotmail.com>
2016-11-29 17:56:33 +0800

    Proximity: add test interface

--- Utilities/Proximity.cs
+++ Utilities/Proximity.cs
@@ -1,9 +1,11 @@
 ﻿using log4net;
+using MissionPlanner.HIL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Windows.Forms;
 using static MAVLink;
 
 namespace MissionPlanner.Utilities
@@ -17,11 +19,80 @@ namespace MissionPlanner.Utilities
 
         KeyValuePair<MAVLINK_MSG_ID, Func<MAVLinkMessage, bool>> sub;
 
+        Form temp = new Form();
+
         public Proximity(MAVState mavInt)
         {
             _parent = mavInt;
             sub = mavInt.parent.SubscribeToPacketType(MAVLINK_MSG_ID.DISTANCE_SENSOR, messageReceived);
             log.InfoFormat("created for {0} - {1}", mavInt.sysid, mavInt.compid);
+
+            temp.Paint += Temp_Paint;
+        }
+
+        private void Temp_Paint(object sender, PaintEventArgs e)
+        {
+            var rawdata = _dS.GetRaw();
+
+            e.Graphics.Clear(temp.BackColor);
+
+            var midx = this.temp.Width / 2.0f;
+            var midy = this.temp.Height / 2.0f;
+
+            e.Graphics.DrawArc(System.Drawing.Pens.Green, midx - 10, midy - 10, 20, 20, 0, 360);
+
+            foreach (var temp in rawdata.ToList())
+            {
+                Vector3 location = new Vector3(0, temp.Distance / 5, 0);
+
+                var halflength = location.length() / 2.0f;
+                var doublelength = location.length() * 2.0f;
+                var length = location.length();
+
+                switch (temp.Orientation)
+                {
+                    case MAV_SENSOR_ORIENTATION.MAV_SENSOR_ROTATION_NONE:
+                        location.rotate(Common.Rotation.ROTATION_NONE);
+                        e.Graphics.DrawString(temp.Distance.ToString(), Control.DefaultFont, System.Drawing.Brushes.Green, midx - (float)location.x, midy - (float)location.y);
+                        e.Graphics.DrawArc(System.Drawing.Pens.Red, (float)(midx - length), (float)(midy - length), (float)doublelength, (float)doublelength, -22.5f - 90f, 45f);
+                        break;
+                    case MAV_SENSOR_ORIENTATION.MAV_SENSOR_ROTATION_YAW_45:
+                        location.rotate(Common.Rotation.ROTATION_YAW_45);
+                        e.Graphics.DrawString(temp.Distance.ToString(), Control.DefaultFont, System.Drawing.Brushes.Green, midx - (float)location.x, midy - (float)location.y);
+                        e.Graphics.DrawArc(System.Drawing.Pens.Red, (float)(midx - length), (float)(midy - length), (float)doublelength, (float)doublelength, 22.5f - 90f, 45f);
+                        break;
+                    case MAV_SENSOR_ORIENTATION.MAV_SENSOR_ROTATION_YAW_90:
+                        location.rotate(Common.Rotation.ROTATION_YAW_90);
+                        e.Graphics.DrawString(temp.Distance.ToString(), Control.DefaultFont, System.Drawing.Brushes.Green, midx - (float)location.x, midy - (float)location.y);
+                        e.Graphics.DrawArc(System.Drawing.Pens.Red, (float)(midx - length), (float)(midy - length), (float)doublelength, (float)doublelength, 67.5f - 90f, 45f);
+                        break;
+                    case MAV_SENSOR_ORIENTATION.MAV_SENSOR_ROTATION_YAW_135:
+                        location.rotate(Common.Rotation.ROTATION_YAW_135);
+                        e.Graphics.DrawString(temp.Distance.ToString(), Control.DefaultFont, System.Drawing.Brushes.Green, midx - (float)location.x, midy - (float)location.y);
+                        e.Graphics.DrawArc(System.Drawing.Pens.Red, (float)(midx - length), (float)(midy - length), (float)doublelength, (float)doublelength, 112.5f - 90f, 45f);
+                        break;
+                    case MAV_SENSOR_ORIENTATION.MAV_SENSOR_ROTATION_YAW_180:
+                        location.rotate(Common.Rotation.ROTATION_YAW_180);
+                        e.Graphics.DrawString(temp.Distance.ToString(), Control.DefaultFont, System.Drawing.Brushes.Green, midx - (float)location.x, midy - (float)location.y);
+                        e.Graphics.DrawArc(System.Drawing.Pens.Red, (float)(midx - length), (float)(midy - length), (float)doublelength, (float)doublelength, 157.5f - 90f, 45f);
+                        break;
+                    case MAV_SENSOR_ORIENTATION.MAV_SENSOR_ROTATION_YAW_225:
+                        location.rotate(Common.Rotation.ROTATION_YAW_225);
+                        e.Graphics.DrawString(temp.Distance.ToString(), Control.DefaultFont, System.Drawing.Brushes.Green, midx - (float)location.x, midy - (float)location.y);
+                        e.Graphics.DrawArc(System.Drawing.Pens.Red, (float)(midx - length), (float)(midy - length), (float)doublelength, (float)doublelength, 202.5f - 90f, 45f);
+                        break;
+                    case MAV_SENSOR_ORIENTATION.MAV_SENSOR_ROTATION_YAW_270:
+                        location.rotate(Common.Rotation.ROTATION_YAW_270);
+                        e.Graphics.DrawString(temp.Distance.ToString(), Control.DefaultFont, System.Drawing.Brushes.Green, midx - (float)location.x, midy - (float)location.y);
+                        e.Graphics.DrawArc(System.Drawing.Pens.Red, (float)(midx - length), (float)(midy - length), (float)doublelength, (float)doublelength, 247.5f - 90f, 45f);
+                        break;
+                    case MAV_SENSOR_ORIENTATION.MAV_SENSOR_ROTATION_YAW_315:
+                        location.rotate(Common.Rotation.ROTATION_YAW_315);
+                        e.Graphics.DrawString(temp.Distance.ToString(), Control.DefaultFont, System.Drawing.Brushes.Green, midx - (float)location.x, midy - (float)location.y);
+                        e.Graphics.DrawArc(System.Drawing.Pens.Red, (float)(midx - length), (float)(midy - length), (float)doublelength, (float)doublelength, 292.5f - 90f, 45f);
+                        break;
+                }
+            }
         }
 
         ~Proximity()
@@ -45,7 +116,23 @@ namespace MissionPlanner.Utilities
                 if (dist.current_distance <= dist.min_distance)
                     return true;
 
-                _dS.Add((MAV_SENSOR_ORIENTATION)dist.orientation, dist.current_distance, DateTime.Now, 1);
+                _dS.Add((MAV_SENSOR_ORIENTATION)dist.orientation, dist.current_distance, DateTime.Now, 3);
+
+                //Console.WriteLine("DIST " + (MAV_SENSOR_ORIENTATION)dist.orientation + " " + dist.current_distance);
+
+                if (temp.IsHandleCreated)
+                {
+                    //temp.Text = arg.sysid.ToString();
+                    temp.Invalidate();
+                    //temp.Refresh();
+                }
+                else
+                {
+                    if (!temp.IsDisposed)
+                    {
+                        MainV2.instance.Invoke((MethodInvoker)delegate { temp.Show(); });
+                    }
+                }
             }
 
             return true;
@@ -75,6 +162,13 @@ namespace MissionPlanner.Utilities
 
             public void Add(MAV_SENSOR_ORIENTATION orientation, double distance, DateTime received, double age = 1)
             {
+                var existing = _dists.Where((a) => { return a.Orientation == orientation; });
+
+                foreach (var item in existing.ToList())
+                {
+                    _dists.Remove(item);
+                }
+
                 _dists.Add(new data(orientation, distance, received, age));
 
                 expire();
