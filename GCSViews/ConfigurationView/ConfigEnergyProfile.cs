2d9fbb6b34b173f1318ee9d6086fa0e54d39205c
Patrick Schneider <patrick.ps.schneider@tu-ilmenau.de>
2016-12-14 18:13:50 +0100

    Files for previous commit (added menu...)

---
+++ GCSViews/ConfigurationView/ConfigEnergyProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using MissionPlanner.Controls;
+using System.Windows.Forms;
+
+namespace MissionPlanner.GCSViews.ConfigurationView
+{
+    public partial class ConfigEnergyProfile : UserControl, IActivate
+    {
+        public ConfigEnergyProfile()
+        {
+            InitializeComponent();
+        }
+
+        public void Activate()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
