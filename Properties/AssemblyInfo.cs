4be399f4de5ed7b8d2f145ae1a395694c06c70ec
Michael Oborne <mich146@hotmail.com>
2017-01-06 06:33:35 +0800

    ConfigTradHeli: update new param names #1444

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -38,7 +38,7 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.3.43.1")]
+[assembly: AssemblyFileVersion("1.3.43.2")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
