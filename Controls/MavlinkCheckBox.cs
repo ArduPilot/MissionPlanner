106f4f1e1f862d298a3616000608b6cb13e3c37e
Michael Oborne <mich146@hotmail.com>
2017-01-06 06:30:38 +0800

    MavlinkCheckbox: add multi paramname support

--- Controls/MavlinkCheckBox.cs
+++ Controls/MavlinkCheckBox.cs
@@ -31,6 +31,19 @@ namespace MissionPlanner.Controls
             this.Enabled = false;
         }
 
+        public void setup(double OnValue, double OffValue, string[] paramname, MAVLink.MAVLinkParamList paramlist,
+            Control enabledisable = null)
+        {
+            foreach (var s in paramname)
+            {
+                if (paramlist.ContainsKey(s))
+                {
+                    setup(OnValue, OffValue, s, paramlist, enabledisable);
+                    return;
+                }
+            }
+        }
+
         public void setup(double OnValue, double OffValue, string paramname, MAVLink.MAVLinkParamList paramlist,
             Control enabledisable = null)
         {
