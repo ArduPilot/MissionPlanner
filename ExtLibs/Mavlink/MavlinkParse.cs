2a2acbc73277887f08b6adef257b302d39f1625a
Michael Oborne <mich146@hotmail.com>
2017-01-09 05:46:54 +0800

    MavlinkParse: add mavlink2 send support

--- ExtLibs/Mavlink/MavlinkParse.cs
+++ ExtLibs/Mavlink/MavlinkParse.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 public partial class MAVLink
@@ -124,7 +125,7 @@ public partial class MAVLink
 
             byte[] packet = new byte[data.Length + 6 + 2];
 
-            packet[0] = 0xfe;
+            packet[0] = MAVLINK_STX_MAVLINK1;
             packet[1] = (byte)data.Length;
             packet[2] = (byte)packetcount;
 
@@ -156,5 +157,118 @@ public partial class MAVLink
 
             return packet;
         }
+
+        public byte[] GenerateMAVLinkPacket20(MAVLINK_MSG_ID messageType, object indata, bool sign = false)
+        {
+            byte[] data;
+
+            data = MavlinkUtil.StructureToByteArray(indata);
+
+            MavlinkUtil.trim_payload(ref data);
+
+            int extra = 0;
+            if (sign)
+                extra = MAVLINK_SIGNATURE_BLOCK_LEN;
+
+            byte[] packet = new byte[data.Length + MAVLINK_NUM_NON_PAYLOAD_BYTES + extra];
+
+            packet[0] = MAVLINK_STX;
+            packet[1] = (byte)data.Length;
+            packet[2] = 0;//incompat  signing
+            if (sign)
+                packet[2] |= MAVLINK_IFLAG_SIGNED;
+            packet[3] = 0;//compat
+            packet[4] = (byte)packetcount;
+
+            packetcount++;
+
+            packet[5] = 255; // this is always 255 - MYGCS
+            packet[6] = (byte)MAV_COMPONENT.MAV_COMP_ID_MISSIONPLANNER;
+            packet[7] = (byte)((UInt32)messageType);
+            packet[8] = (byte)((UInt32)messageType >> 8);
+            packet[9] = (byte)((UInt32)messageType >> 16);
+
+            int i = MAVLINK_NUM_HEADER_BYTES;
+            foreach (byte b in data)
+            {
+                packet[i] = b;
+                i++;
+            }
+
+            ushort checksum = MavlinkCRC.crc_calculate(packet, data.Length + MAVLINK_NUM_HEADER_BYTES);
+
+            checksum = MavlinkCRC.crc_accumulate(MAVLINK_MESSAGE_INFOS.GetMessageInfo((uint)messageType).crc, checksum);
+
+            byte ck_a = (byte)(checksum & 0xFF); ///< High byte
+            byte ck_b = (byte)(checksum >> 8); ///< Low byte
+
+            packet[i] = ck_a;
+            i += 1;
+            packet[i] = ck_b;
+            i += 1;
+
+            if (sign)
+            {
+                //https://docs.google.com/document/d/1ETle6qQRcaNWAmpG2wz0oOpFKSF_bcTmYMQvtTGI8ns/edit
+
+                /*
+                8 bits of link ID
+                48 bits of timestamp
+                48 bits of signature
+                */
+
+                // signature = sha256_48(secret_key + header + payload + CRC + link-ID + timestamp)
+
+                var timestamp = (UInt64)((DateTime.UtcNow - new DateTime(2015, 1, 1)).TotalMilliseconds * 100);
+
+                if (timestamp == lasttimestamp)
+                    timestamp++;
+
+                lasttimestamp = timestamp;
+
+                var timebytes = BitConverter.GetBytes(timestamp);
+
+                var sig = new byte[7]; // 13 includes the outgoing hash
+                sig[0] = sendlinkid;
+                Array.Copy(timebytes, 0, sig, 1, 6); // timestamp
+
+                //Console.WriteLine("gen linkid {0}, time {1} {2} {3} {4} {5} {6} {7}", sig[0], sig[1], sig[2], sig[3], sig[4], sig[5], sig[6], timestamp);
+                
+                if (signingKey == null || signingKey.Length != 32)
+                {
+                    signingKey = new byte[32];
+                }
+
+                using (SHA256Managed signit = new SHA256Managed())
+                {
+                    signit.TransformBlock(signingKey, 0, signingKey.Length, null, 0);
+                    signit.TransformBlock(packet, 0, i, null, 0);
+                    signit.TransformFinalBlock(sig, 0, sig.Length);
+                    var ctx = signit.Hash;
+                    // trim to 48
+                    Array.Resize(ref ctx, 6);
+
+                    foreach (byte b in sig)
+                    {
+                        packet[i] = b;
+                        i++;
+                    }
+
+                    foreach (byte b in ctx)
+                    {
+                        packet[i] = b;
+                        i++;
+                    }
+                }
+            }
+
+            return packet;
+        }
+
+        public byte sendlinkid { get; set; }
+
+        public ulong lasttimestamp { get; set; }
+
+        public byte[] signingKey { get; set; }
     }
 }
