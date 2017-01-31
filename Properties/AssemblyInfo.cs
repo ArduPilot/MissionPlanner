ad94f100db3f1ecda62d11009df2ca1c2b973f01
Michael Oborne <mich146@hotmail.com>
2017-01-31 20:26:00 +0800

    DFLogScript: fix earth_accel_df

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -38,7 +38,7 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.3.44")]
+[assembly: AssemblyFileVersion("1.3.44.1")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
