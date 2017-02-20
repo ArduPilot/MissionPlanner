199e1202f3e9bf5b2f90fb06dbc47c02340c8941
Patrick Schneider <patrick.ps.schneider@tu-ilmenau.de>
2017-02-20 15:15:58 +0100

    Added proper errorhandling. fixed xmldocument parsing (added root element)

--- GCSViews/ConfigurationView/ConfigEnergyProfile.cs
+++ GCSViews/ConfigurationView/ConfigEnergyProfile.cs
@@ -24,7 +24,15 @@ namespace MissionPlanner.GCSViews.ConfigurationView
         public void Activate()
         {
             CB_EnableEnergyProfile.Checked = Convert.ToBoolean(Settings.Instance["EnergyProfileEnabled"]);
-            if(MainV2.comPort.BaseStream.IsOpen)
+            if (CB_EnableEnergyProfile.Checked == true)
+            {
+                LoadCopterSettings();
+            }
+        }
+
+        private void LoadCopterSettings()   //Energyprofile for vehicle
+        {
+            if (MainV2.comPort.BaseStream.IsOpen)
             {
                 if (MainV2.comPort.MAV.param.ContainsKey("BRD_SERIAL_NUM"))
                 {
@@ -35,28 +43,39 @@ namespace MissionPlanner.GCSViews.ConfigurationView
                     {
                         using (System.Xml.XmlReader xr = System.Xml.XmlReader.Create(EnergyProfilePath + tbCopterID.Text + ".xml"))
                         {
-                            xr.ReadToFollowing("Current");
-                            tbAmpINeg.Text = xr.GetAttribute("NegAmp");
-                            tbAngINeg.Text = xr.GetAttribute("NegAmpPosition");
-                            tbVarINeg.Text = xr.GetAttribute("NegVariance");
-                            tbAmpIPos.Text = xr.GetAttribute("PosAmp");
-                            tbAngIPos.Text = xr.GetAttribute("PosAmpPosition");
-                            tbVarIPos.Text = xr.GetAttribute("PosVariance");
-                            tbDeviationMax.Text = xr.GetAttribute("MaxDeviation");
-                            tbDeviationMin.Text = xr.GetAttribute("MinDeviation");
-                            tbLimitI.Text = xr.GetAttribute("LowerLimit");
-                            tbHoverI.Text = xr.GetAttribute("CurrentHover");
-                            
-                            //xw.WriteStartElement("Velocity");
-                            //xw.WriteAttributeString("Amplitude", tbAmpV.Text);
-                            //xw.WriteAttributeString("AmpPosition", tbAngV.Text);
-                            //xw.WriteAttributeString("Variance", tbVarV.Text);
-                            //xw.WriteAttributeString("LowerBound", tbLowerAmp.Text);
-                            //xw.WriteAttributeString("Curvature", tbCurvatureV.Text);
-                            //xw.WriteAttributeString("Gradient", tbGradientV.Text);
+                            try
+                            {
+                                xr.ReadToFollowing("EnergyProfile");
+                                xr.ReadToFollowing("Current");
+                                tbAmpINeg.Text = xr.GetAttribute("NegAmp");
+                                tbAngINeg.Text = xr.GetAttribute("NegAmpPosition");
+                                tbVarINeg.Text = xr.GetAttribute("NegVariance");
+                                tbAmpIPos.Text = xr.GetAttribute("PosAmp");
+                                tbAngIPos.Text = xr.GetAttribute("PosAmpPosition");
+                                tbVarIPos.Text = xr.GetAttribute("PosVariance");
+                                tbDeviationMax.Text = xr.GetAttribute("MaxDeviation");
+                                tbDeviationMin.Text = xr.GetAttribute("MinDeviation");
+                                tbLimitI.Text = xr.GetAttribute("LowerLimit");
+                                tbHoverI.Text = xr.GetAttribute("CurrentHover");
+
+                                xr.ReadToFollowing("Velocity");
+                                tbAmpV.Text = xr.GetAttribute("Amplitude");
+                                tbAngV.Text = xr.GetAttribute("AmpPosition");
+                                tbVarV.Text = xr.GetAttribute("Variance");
+                                tbLowerAmp.Text = xr.GetAttribute("LowerBound");
+                                tbCurvatureV.Text = xr.GetAttribute("Curvature");
+                                tbGradientV.Text = xr.GetAttribute("Gradient");
+                                xr.Close();
+
+                                PlotVelocity();
+                                PlotCurrent();
+                            }
+                            catch (System.Xml.XmlException)
+                            {
+                                CustomMessageBox.Show("Error reading XmlFile");
+                            }
                         }
                     }
-                        
                 }
             }
         }
@@ -82,19 +101,127 @@ namespace MissionPlanner.GCSViews.ConfigurationView
 
         private void btnPlotI_Click(object sender, EventArgs e)
         {
-            double.TryParse(tbAmpINeg.Text, out dAmplitudeNegI);
-            double.TryParse(tbAngINeg.Text, out dAngleNegI);
-            double.TryParse(tbVarINeg.Text, out dDeviationNegI);
-            double.TryParse(tbAmpIPos.Text, out dAmplitudePosI);
-            double.TryParse(tbAngIPos.Text, out dAnglePosI);
-            double.TryParse(tbVarIPos.Text, out dDeviationPosI);
-            double.TryParse(tbLimitI.Text, out dLowerLimitI);
+            PlotCurrent();
+        }
+
+        private void btnSaveCopterSettings_Click(object sender, EventArgs e)
+        {
+            if (MainV2.comPort.GetParam("BRD_SERIAL_NUM") != 0 && tbCopterID.Text != string.Empty)
+            {
+                MainV2.comPort.setParam("BRD_SERIAL_NUM", double.Parse(tbCopterID.Text));
+
+                //Save parameters to xml file
+                if (!(Directory.Exists(EnergyProfilePath)))
+                {
+                    Directory.CreateDirectory(EnergyProfilePath);
+                }
+
+                using (System.Xml.XmlWriter xw = System.Xml.XmlWriter.Create(EnergyProfilePath + tbCopterID.Text + ".xml"))
+                {
+                    xw.WriteStartDocument();
+                    xw.WriteStartElement("EnergyProfile");
+                    xw.WriteStartElement("Current");
+                    xw.WriteAttributeString("NegAmp", tbAmpINeg.Text);
+                    xw.WriteAttributeString("NegAmpPosition", tbAngINeg.Text);
+                    xw.WriteAttributeString("NegVariance", tbVarINeg.Text);
+                    xw.WriteAttributeString("PosAmp", tbAmpIPos.Text);
+                    xw.WriteAttributeString("PosAmpPosition", tbAngIPos.Text);
+                    xw.WriteAttributeString("PosVariance", tbVarIPos.Text);
+                    xw.WriteAttributeString("MaxDeviation", tbDeviationMax.Text);
+                    xw.WriteAttributeString("MinDeviation", tbDeviationMin.Text);
+                    xw.WriteAttributeString("LowerLimit", tbLimitI.Text);
+                    xw.WriteAttributeString("CurrentHover", tbHoverI.Text);
+                    xw.WriteEndElement();
+
+                    xw.WriteStartElement("Velocity");
+                    xw.WriteAttributeString("Amplitude", tbAmpV.Text);
+                    xw.WriteAttributeString("AmpPosition", tbAngV.Text);
+                    xw.WriteAttributeString("Variance", tbVarV.Text);
+                    xw.WriteAttributeString("LowerBound", tbLowerAmp.Text);
+                    xw.WriteAttributeString("Curvature", tbCurvatureV.Text);
+                    xw.WriteAttributeString("Gradient", tbGradientV.Text);
+                    xw.WriteEndElement();
+
+                    xw.WriteEndElement();
+                    xw.WriteEndDocument();
+                    xw.Close();
+                }
+            }
+        }
+
+        private bool ParseVelocityValues()
+        {
+            try
+            {
+                dAmplitudeV = double.Parse(tbAmpV.Text);
+                dLowerAmpV = double.Parse(tbLowerAmp.Text);
+                dAngleV = double.Parse(tbAngV.Text);
+                dDeviationV = double.Parse(tbVarV.Text);
+                dCurvatureV = double.Parse(tbCurvatureV.Text);
+                dGradientV = double.Parse(tbGradientV.Text);
+            }
+            catch (FormatException fe)
+            {
+                CustomMessageBox.Show("Velocity: Invalid format or textbox empty!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ParseCurrentValues()
+        {
+            try
+            {
+                dAmplitudeNegI = double.Parse(tbAmpINeg.Text);
+                dAngleNegI = double.Parse(tbAngINeg.Text);
+                dDeviationNegI = double.Parse(tbVarINeg.Text);
+                dAmplitudePosI = double.Parse(tbAmpIPos.Text);
+                dAnglePosI = double.Parse(tbAngIPos.Text);
+                dDeviationPosI = double.Parse(tbVarIPos.Text);
+                dLowerLimitI = double.Parse(tbLimitI.Text);
+                dHover = double.Parse(tbHoverI.Text);
+                dDevMax = double.Parse(tbDeviationMax.Text);
+                dDevMin = double.Parse(tbDeviationMin.Text);
+            }
+            catch (FormatException)
+            {
+                CustomMessageBox.Show("Current: Invalid format or textbox empty!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private double dAmplitudeV, dLowerAmpV, dAngleV, dDeviationV, dCurvatureV, dGradientV;
+        private double dAmplitudeNegI, dAngleNegI, dDeviationNegI, dAmplitudePosI, dAnglePosI, dDeviationPosI, dLowerLimitI, dHover, dDevMax, dDevMin;
+
+        private void btnPlotV_Click(object sender, EventArgs e)
+        {
+            PlotVelocity();
+        }
+
+        private void PlotVelocity()
+        {
+            if (!ParseVelocityValues()) { return; }
+
+            ChartV.Series[0].Points.Clear();
+
+            for (double i = -90; i <= 90; i += 5)
+            {
+                ChartV.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(i, PolyValV(i)));
+            }
+        }
+
+        private void PlotCurrent()
+        {
+            if (!ParseCurrentValues()) { return; }
 
             ChartI.Series[0].Points.Clear();
             ChartI.Series[1].Points.Clear();
             ChartI.Series[2].Points.Clear();
 
-            for (double i = -90; i <= 90; i+=5)
+            for (double i = -90; i <= 90; i += 5)
             {
                 ChartI.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(i, PolyValI(i)));
                 ChartI.Series[1].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(i, PolyValI(i, double.Parse(tbDeviationMax.Text))));
@@ -107,69 +234,6 @@ namespace MissionPlanner.GCSViews.ConfigurationView
 
             //(a1 - t) ℯ^((-0.5(x - b1)²) / (2c1²)) + (a2 - t) ℯ^((-0.5(x - b2)²) / (2c2²)) + t
         }
-
-        private double dAmplitudeNegI, dAngleNegI, dDeviationNegI, dAmplitudePosI, dAnglePosI, dDeviationPosI, dLowerLimitI;
-
-        private void btnSaveCopterSettings_Click(object sender, EventArgs e)
-        {
-            if (MainV2.comPort.GetParam("BRD_SERIAL_NUM") != 0 && tbCopterID.Text != string.Empty)
-            {
-                MainV2.comPort.setParam("BRD_SERIAL_NUM", double.Parse(tbCopterID.Text));
-
-                //Save parameters to xml file
-
-                if (!(Directory.Exists(EnergyProfilePath)))
-                {
-                    Directory.CreateDirectory(EnergyProfilePath);
-                }
-
-                using (System.Xml.XmlWriter xw = System.Xml.XmlWriter.Create(EnergyProfilePath + tbCopterID.Text + ".xml"))
-                {
-                    xw.WriteStartElement("Current");
-                        xw.WriteAttributeString("NegAmp", tbAmpINeg.Text);
-                        xw.WriteAttributeString("NegAmpPosition", tbAmpIPos.Text);
-                        xw.WriteAttributeString("NegVariance", tbVarINeg.Text);
-                        xw.WriteAttributeString("PosAmp", tbAmpIPos.Text);
-                        xw.WriteAttributeString("PosAmpPosition", tbAngIPos.Text);
-                        xw.WriteAttributeString("PosVariance", tbVarIPos.Text);
-                        xw.WriteAttributeString("MaxDeviation", tbDeviationMax.Text);
-                        xw.WriteAttributeString("MinDeviation", tbDeviationMin.Text);
-                        xw.WriteAttributeString("LowerLimit", tbLimitI.Text);
-                        xw.WriteAttributeString("CurrentHover", tbHoverI.Text);
-                    xw.WriteEndElement();
-
-                    /*xw.WriteStartElement("Velocity");
-                        xw.WriteAttributeString("Amplitude", tbAmpV.Text);
-                        xw.WriteAttributeString("AmpPosition", tbAngV.Text);
-                        xw.WriteAttributeString("Variance", tbVarV.Text);
-                        xw.WriteAttributeString("LowerBound", tbLowerAmp.Text);
-                        xw.WriteAttributeString("Curvature", tbCurvatureV.Text);
-                        xw.WriteAttributeString("Gradient", tbGradientV.Text);
-                    xw.WriteEndElement();*/
-                    xw.Close();
-                }
-            }
-        }
-
-        private double dAmplitudeV, dLowerAmpV, dAngleV, dDeviationV, dCurvatureV, dGradientV;
-
-        private void btnPlotV_Click(object sender, EventArgs e)
-        {
-            double.TryParse(tbAmpV.Text, out dAmplitudeV);
-            double.TryParse(tbLowerAmp.Text, out dLowerAmpV);
-            double.TryParse(tbAngV.Text, out dAngleV);
-            double.TryParse(tbVarV.Text, out dDeviationV);
-            double.TryParse(tbCurvatureV.Text, out dCurvatureV);
-            double.TryParse(tbGradientV.Text, out dGradientV);
-
-            ChartV.Series[0].Points.Clear();
-
-            for (double i = -90; i <= 90; i+=5)
-            {
-                ChartV.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(i, PolyValV(i)));
-            }
-        }
-
 
         private double PolyValV(double _dAngle)
         {
