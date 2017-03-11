e1d9e6f5eddae572dc20ad22c82dccca0ec79032
Michael Oborne <mich146@hotmail.com>
2017-03-11 14:26:24 +0800

    ConfigMotorTest: add to groupbox

--- GCSViews/ConfigurationView/ConfigMotorTest.cs
+++ GCSViews/ConfigurationView/ConfigMotorTest.cs
@@ -49,7 +49,7 @@ namespace MissionPlanner.GCSViews.ConfigurationView
                 but.Click += but_Click;
                 but.Tag = a;
 
-                Controls.Add(but);
+                groupBox1.Controls.Add(but);
 
                 y += 25;
             }
@@ -59,7 +59,7 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             but.Location = new Point(x, y);
             but.Size = new Size(75, 37);
             but.Click += but_TestAll;
-            Controls.Add(but);
+            groupBox1.Controls.Add(but);
 
             y += 39;
 
@@ -68,7 +68,7 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             but.Location = new Point(x, y);
             but.Size = new Size(75, 37);
             but.Click += but_StopAll;
-            Controls.Add(but);
+            groupBox1.Controls.Add(but);
 
             y += 39;
 
@@ -77,7 +77,7 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             but.Location = new Point(x, y);
             but.Size = new Size(75, 37);
             but.Click += but_TestAllSeq;
-            Controls.Add(but);
+            groupBox1.Controls.Add(but);
 
             Utilities.ThemeManager.ApplyThemeTo(this);
         }
