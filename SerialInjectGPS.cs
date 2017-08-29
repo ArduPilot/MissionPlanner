4dab013293326febb0358219a7ab064258c3ebb3
chobits <chobits@itri.org.tw>
2017-08-29 17:11:11 +0800

    fix tab

--- SerialInjectGPS.cs
+++ SerialInjectGPS.cs
@@ -132,7 +132,7 @@ namespace MissionPlanner
             {
                 List<rtcm3.ob> obs = sender as List<rtcm3.ob>;
 
-				if (obs.Count == 0) return;
+                if (obs.Count == 0) return;
 				
                 // get system controls
                 Func<char,List<VerticalProgressBar2>> ctls = delegate (char sys)
