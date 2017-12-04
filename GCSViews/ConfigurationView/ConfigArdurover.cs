161b1de0e897f1cb8bde59775d84007c86abe4f7
Daniel Widmann <widmann.daniel@gmail.com>
2017-12-04 23:17:04 +0100

    ConfigArdurover: Add tuning options for 3.2

--- GCSViews/ConfigurationView/ConfigArdurover.cs
+++ GCSViews/ConfigurationView/ConfigArdurover.cs
@@ -42,28 +42,35 @@ namespace MissionPlanner.GCSViews.ConfigurationView
 
             startup = true;
 
-            FS_THR_VALUE.setup(0, 0, 1, 1, "FS_THR_VALUE", MainV2.comPort.MAV.param);
-            THR_MAX.setup(0, 0, 1, 1, "THR_MAX", MainV2.comPort.MAV.param);
-            THR_MIN.setup(0, 0, 1, 1, "THR_MIN", MainV2.comPort.MAV.param);
+            STEER2SRV_P.setup(0, 0, 1, 0.1f, "ATC_STR_RAT_P", MainV2.comPort.MAV.param);
+            STEER2SRV_I.setup(0, 0, 1, 0.1f, "ATC_STR_RAT_I", MainV2.comPort.MAV.param);
+            STEER2SRV_D.setup(0, 0, 1, 0.1f, "ATC_STR_RAT_D", MainV2.comPort.MAV.param);
+            STEER2SRV_IMAX.setup(0, 0, 1, 0.1f, "ATC_STR_RAT_IMAX", MainV2.comPort.MAV.param);
+
+            TURN_RADIUS.setup(0, 0, 1, 0.1f, "TURN_RADIUS", MainV2.comPort.MAV.param);
+
+            SPEED2THR_P.setup(0, 0, 1, 0.1f, "ATC_SPEED_P", MainV2.comPort.MAV.param);
+            SPEED2THR_I.setup(0, 0, 1, 0.1f, "ATC_SPEED_I", MainV2.comPort.MAV.param);
+            SPEED2THR_D.setup(0, 0, 1, 0.1f, "ATC_SPEED_D", MainV2.comPort.MAV.param);
+            SPEED2THR_IMAX.setup(0, 0, 1, 0.1f, "ATC_SPEED_IMAX", MainV2.comPort.MAV.param);
+            ATC_ACCEL_MAX.setup(0, 0, 1, 0.1f, "ATC_ACCEL_MAX", MainV2.comPort.MAV.param);
+
+            CRUISE_SPEED.setup(0, 0, 1, 0.1f, "CRUISE_SPEED", MainV2.comPort.MAV.param);
             CRUISE_THROTTLE.setup(0, 0, 1, 1, "CRUISE_THROTTLE", MainV2.comPort.MAV.param);
-            SPEED2THR_IMAX.setup(0, 0, 1, 1, "SPEED2THR_IMAX", MainV2.comPort.MAV.param);
-            SPEED2THR_D.setup(0, 0, 1, 1, "SPEED2THR_D", MainV2.comPort.MAV.param);
-            SPEED2THR_I.setup(0, 0, 1, 1, "SPEED2THR_I", MainV2.comPort.MAV.param);
-            SPEED2THR_P.setup(0, 0, 1, 1, "SPEED2THR_P", MainV2.comPort.MAV.param);
-            SPEED_TURN_DIST.setup(0, 0, 1, 1, "SPEED_TURN_DIST", MainV2.comPort.MAV.param);
-            SPEED_TURN_GAIN.setup(0, 0, 1, 1, "SPEED_TURN_GAIN", MainV2.comPort.MAV.param);
-            CRUISE_SPEED.setup(0, 0, 1, 1, "CRUISE_SPEED", MainV2.comPort.MAV.param);
-            STEER2SRV_IMAX.setup(0, 0, 1, 1, "STEER2SRV_IMAX", MainV2.comPort.MAV.param);
-            STEER2SRV_D.setup(0, 0, 1, 1, "STEER2SRV_D", MainV2.comPort.MAV.param);
-            STEER2SRV_I.setup(0, 0, 1, 1, "STEER2SRV_I", MainV2.comPort.MAV.param);
-            STEER2SRV_P.setup(0, 0, 1, 1, "STEER2SRV_P", MainV2.comPort.MAV.param);
-            SONAR_DEBOUNCE.setup(0, 0, 1, 1, "SONAR_DEBOUNCE", MainV2.comPort.MAV.param);
-            SONAR_TURN_TIME.setup(0, 0, 1, 1, "SONAR_TURN_TIME", MainV2.comPort.MAV.param);
-            SONAR_TURN_ANGLE.setup(0, 0, 1, 1, "SONAR_TURN_ANGLE", MainV2.comPort.MAV.param);
-            SONAR_TRIGGER_CM.setup(0, 0, 1, 1, "SONAR_TRIGGER_CM", MainV2.comPort.MAV.param);
-            WP_RADIUS.setup(0, 0, 1, 1, "WP_RADIUS", MainV2.comPort.MAV.param);
-            NAVL1_DAMPING.setup(0, 0, 1, 1, "NAVL1_DAMPING", MainV2.comPort.MAV.param);
+            THR_MIN.setup(0, 0, 1, 1, "MOT_THR_MIN", MainV2.comPort.MAV.param);
+            THR_MAX.setup(0, 0, 1, 1, "MOT_THR_MAX", MainV2.comPort.MAV.param);
+            MOT_SKID_FRIC.setup(0, 0, 1, 1, "MOT_SKID_FRIC", MainV2.comPort.MAV.param);
+
+            WP_RADIUS.setup(0, 0, 1, 0.1f, "WP_RADIUS", MainV2.comPort.MAV.param);
+            WP_OVERSHOOT.setup(0, 0, 1, 0.1f, "WP_OVERSHOOT", MainV2.comPort.MAV.param);
+            TURN_G_MAX.setup(0, 0, 1, 1, "TURN_MAX_G", MainV2.comPort.MAV.param);      
             NAVL1_PERIOD.setup(0, 0, 1, 1, "NAVL1_PERIOD", MainV2.comPort.MAV.param);
+            NAVL1_DAMPING.setup(0, 0, 1, 0.05f, "NAVL1_DAMPING", MainV2.comPort.MAV.param);
+
+            SONAR_TRIGGER_CM.setup(0, 0, 1, 1, "RNGFND_TRIGGR_CM", MainV2.comPort.MAV.param);
+            SONAR_TURN_ANGLE.setup(0, 0, 1, 1, "RNGFND_TURN_ANGL", MainV2.comPort.MAV.param);
+            SONAR_TURN_TIME.setup(0, 0, 1, 1, "RNGFND_TURN_TIME", MainV2.comPort.MAV.param);
+            SONAR_DEBOUNCE.setup(0, 0, 1, 1, "RNGFND_DEBOUNCE", MainV2.comPort.MAV.param);
 
             changes.Clear();
 
