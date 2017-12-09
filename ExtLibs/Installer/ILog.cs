08cd2d98c9bad59fdb0cfa01761faa7cfe1994cd
Michael Oborne <mich146@hotmail.com>
2017-12-09 13:26:18 +0800

    Installer: create

---
+++ ExtLibs/Installer/ILog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace log4net
+{
+    internal interface ILog
+    {
+        void Debug(string v);
+        void Info(string v);
+    }
+
+    internal class Log : ILog
+    {
+        public void Debug(string v)
+        {
+            Console.WriteLine(v);
+        }
+
+        public void Info(string v)
+        {
+            Console.WriteLine(v);
+        }
+    }
+}
