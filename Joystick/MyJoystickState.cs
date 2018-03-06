1c70fe61ce69329d3187a20d02fabc21d83f97c7
Michael Oborne <mich146@hotmail.com>
2018-03-06 10:14:13 +0800

    Joystick: seperate class

---
+++ Joystick/MyJoystickState.cs
@@ -0,0 +1,149 @@
+﻿using SharpDX.DirectInput;
+
+namespace MissionPlanner.Joystick
+{
+    public class MyJoystickState
+    {
+        internal JoystickState baseJoystickState;
+
+        public MyJoystickState(JoystickState state)
+        {
+            baseJoystickState = state;
+        }
+
+        public int[] GetSlider()
+        {
+            return baseJoystickState.Sliders;
+        }
+
+        public int[] GetPointOfView()
+        {
+            return baseJoystickState.PointOfViewControllers;
+        }
+
+        public bool[] GetButtons()
+        {
+            return baseJoystickState.Buttons;
+        }
+
+        public int AZ
+        {
+            get { return baseJoystickState.AccelerationZ; }
+        }
+
+        public int AY
+        {
+            get { return baseJoystickState.AccelerationY; }
+        }
+
+        public int AX
+        {
+            get { return baseJoystickState.AccelerationX; }
+        }
+
+        public int ARz
+        {
+            get { return baseJoystickState.AngularAccelerationZ; }
+        }
+
+        public int ARy
+        {
+            get { return baseJoystickState.AngularAccelerationY; }
+        }
+
+        public int ARx
+        {
+            get { return baseJoystickState.AngularAccelerationX; }
+        }
+
+        public int FRx
+        {
+            get { return baseJoystickState.TorqueX; }
+        }
+
+        public int FRy
+        {
+            get { return baseJoystickState.TorqueY; }
+        }
+
+        public int FRz
+        {
+            get { return baseJoystickState.TorqueZ; }
+        }
+
+        public int FX
+        {
+            get { return baseJoystickState.ForceX; }
+        }
+
+        public int FY
+        {
+            get { return baseJoystickState.ForceY; }
+        }
+
+        public int FZ
+        {
+            get { return baseJoystickState.ForceZ; }
+        }
+
+        public int Rx
+        {
+            get { return baseJoystickState.RotationX; }
+        }
+
+        public int Ry
+        {
+            get { return baseJoystickState.RotationY; }
+        }
+
+        public int Rz
+        {
+            get { return baseJoystickState.RotationZ; }
+        }
+
+        public int VRx
+        {
+            get { return baseJoystickState.AngularVelocityX; }
+        }
+
+        public int VRy
+        {
+            get { return baseJoystickState.AngularVelocityY; }
+        }
+
+        public int VRz
+        {
+            get { return baseJoystickState.AngularVelocityZ; }
+        }
+
+        public int VX
+        {
+            get { return baseJoystickState.VelocityX; }
+        }
+
+        public int VY
+        {
+            get { return baseJoystickState.VelocityY; }
+        }
+
+        public int VZ
+        {
+            get { return baseJoystickState.VelocityZ; }
+        }
+
+        public int X
+        {
+            get { return baseJoystickState.X; }
+        }
+
+        public int Y
+        {
+            get { return baseJoystickState.Y; }
+        }
+
+        public int Z
+        {
+            get { return baseJoystickState.Z; }
+        }
+    }
+}
