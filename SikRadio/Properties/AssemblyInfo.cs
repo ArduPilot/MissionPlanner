0f5bae054c0a170dc21b6f1cf87d202c2422006d
Steven Higginbotham <stevenh@rfdesign.com.au>
2018-05-02 15:43:02 +1000

    RFD900Tools 2.9

--- SikRadio/Properties/AssemblyInfo.cs
+++ SikRadio/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.8.0")]
-[assembly: AssemblyFileVersion("0.2.8.0")]
+[assembly: AssemblyVersion("0.2.9.0")]
+[assembly: AssemblyFileVersion("0.2.9.0")]
