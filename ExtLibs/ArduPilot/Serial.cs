6969dce9c66c1f3c217aaf205e00424bbe20cfd6
Michael Oborne <mich146@hotmail.com>
2018-07-22 21:05:15 +0800

    CurrentState: console write mavlink_autopilot_version_t

---
+++ ExtLibs/ArduPilot/Serial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace MissionPlanner
+{
+    public class Serial
+    {
+        public static void print(object v)
+        {
+            if (v.GetType().IsArray)
+                Console.Write(ASCIIEncoding.ASCII.GetString((byte[])v));
+            else
+                Console.Write(v);
+        }
+        public static void println(object v)
+        {
+            if (v.GetType().IsArray)
+                Console.WriteLine(ASCIIEncoding.ASCII.GetString((byte[])v));
+            else
+                Console.WriteLine(v);
+        }
+    }
+}
