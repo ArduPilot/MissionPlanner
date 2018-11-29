7f5f8ed10f64e4af182d51a14ddef8723d9aee0f
Michael Oborne <mich146@hotmail.com>
2018-11-29 20:57:08 +0800

    ConfigPlanner: save rates between connects #2009

--- GCSViews/ConfigurationView/ConfigPlanner.cs
+++ GCSViews/ConfigurationView/ConfigPlanner.cs
@@ -504,6 +504,8 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             Settings.Instance[((ComboBox) sender).Name] = ((ComboBox) sender).Text;
             MainV2.comPort.MAV.cs.rateattitude = int.Parse(((ComboBox) sender).Text);
 
+            CurrentState.rateattitudebackup = MainV2.comPort.MAV.cs.rateattitude;
+
             MainV2.comPort.requestDatastream(MAVLink.MAV_DATA_STREAM.EXTRA1, MainV2.comPort.MAV.cs.rateattitude);
             // request attitude
             MainV2.comPort.requestDatastream(MAVLink.MAV_DATA_STREAM.EXTRA2, MainV2.comPort.MAV.cs.rateattitude);
@@ -517,6 +519,8 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             Settings.Instance[((ComboBox) sender).Name] = ((ComboBox) sender).Text;
             MainV2.comPort.MAV.cs.rateposition = int.Parse(((ComboBox) sender).Text);
 
+            CurrentState.ratepositionbackup = MainV2.comPort.MAV.cs.rateposition;
+
             MainV2.comPort.requestDatastream(MAVLink.MAV_DATA_STREAM.POSITION, MainV2.comPort.MAV.cs.rateposition);
             // request gps
         }
@@ -527,6 +531,8 @@ namespace MissionPlanner.GCSViews.ConfigurationView
                 return;
             Settings.Instance[((ComboBox) sender).Name] = ((ComboBox) sender).Text;
             MainV2.comPort.MAV.cs.ratestatus = int.Parse(((ComboBox) sender).Text);
+
+            CurrentState.ratestatusbackup = MainV2.comPort.MAV.cs.ratestatus;
 
             MainV2.comPort.requestDatastream(MAVLink.MAV_DATA_STREAM.EXTENDED_STATUS, MainV2.comPort.MAV.cs.ratestatus);
             // mode
@@ -539,6 +545,8 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             Settings.Instance[((ComboBox) sender).Name] = ((ComboBox) sender).Text;
             MainV2.comPort.MAV.cs.raterc = int.Parse(((ComboBox) sender).Text);
 
+            CurrentState.ratercbackup = MainV2.comPort.MAV.cs.raterc;
+
             MainV2.comPort.requestDatastream(MAVLink.MAV_DATA_STREAM.RC_CHANNELS, MainV2.comPort.MAV.cs.raterc);
             // request rc info 
         }
@@ -549,6 +557,8 @@ namespace MissionPlanner.GCSViews.ConfigurationView
                 return;
             Settings.Instance[((ComboBox) sender).Name] = ((ComboBox) sender).Text;
             MainV2.comPort.MAV.cs.ratesensors = int.Parse(((ComboBox) sender).Text);
+
+            CurrentState.ratesensorsbackup = MainV2.comPort.MAV.cs.ratesensors;
 
             MainV2.comPort.requestDatastream(MAVLink.MAV_DATA_STREAM.EXTRA3, MainV2.comPort.MAV.cs.ratesensors);
             // request extra stuff - tridge
