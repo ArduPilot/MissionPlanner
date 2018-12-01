9294b17c5f47eb57335a3c530b089092d808e44f
Michael Oborne <mich146@hotmail.com>
2018-12-01 20:21:39 +0800

    ConfigFirmware: and extra solo prompt

--- GCSViews/ConfigurationView/ConfigFirmware.cs
+++ GCSViews/ConfigurationView/ConfigFirmware.cs
@@ -410,7 +410,9 @@ namespace MissionPlanner.GCSViews.ConfigurationView
                     {
                         if (fd.FileName.ToLower().EndsWith(".px4") || fd.FileName.ToLower().EndsWith(".apj"))
                         {
-                            if (solo.Solo.is_solo_alive)
+                            if (solo.Solo.is_solo_alive && 
+                                solo.Solo.is_controller_alive && 
+                                CustomMessageBox.Show("Solo","Is this a Solo?",CustomMessageBox.MessageBoxButtons.YesNo) == CustomMessageBox.DialogResult.Yes)
                             {
                                 boardtype = BoardDetect.boards.solo;
                             }
