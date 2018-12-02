5524710f9d1180c0c1afa0bffd24c74e2b7ffc3a
Michael Oborne <mich146@hotmail.com>
2018-12-02 10:12:16 +0800

    Grid: fix line selection #1679

--- ExtLibs/Utilities/Grid.cs
+++ ExtLibs/Utilities/Grid.cs
@@ -719,8 +719,30 @@ namespace MissionPlanner.Utilities
 
         static linelatlng findClosestLine(utmpos start, List<linelatlng> list, double minDistance, double angle)
         {
+            if (minDistance == 0)
+            {
+                linelatlng answer = list[0];
+                double shortest = double.MaxValue;
+
+                foreach (linelatlng line in list)
+                {
+                    double ans1 = start.GetDistance(line.p1);
+                    double ans2 = start.GetDistance(line.p2);
+                    utmpos shorterpnt = ans1 < ans2 ? line.p1 : line.p2;
+
+                    if (shortest > start.GetDistance(shorterpnt))
+                    {
+                        answer = line;
+                        shortest = start.GetDistance(shorterpnt);
+                    }
+                }
+
+                return answer;
+            }
+
+
             // By now, just add 5.000 km to our lines so they are long enough to allow intersection
-            double METERS_TO_EXTEND = 5000000;
+            double METERS_TO_EXTEND = 5000;
 
             double perperndicularOrientation = AddAngle(angle, 90);
 
@@ -745,17 +767,15 @@ namespace MissionPlanner.Utilities
 
             foreach (linelatlng line in list)
             {
-                // Extend line at both ends so it intersecs for sure with our perpendicular line
-                utmpos extended_line_start = newpos(line.p1, angle, -METERS_TO_EXTEND);
-                utmpos extended_line_stop = newpos(line.p2, angle, METERS_TO_EXTEND);
                 // Calculate intersection point
-                utmpos p = FindLineIntersection(extended_line_start, extended_line_stop, start_perpendicular_line, stop_perpendicular_line);
+                utmpos p = FindLineIntersectionExtension(line.p1, line.p2, start_perpendicular_line, stop_perpendicular_line);
                 
                 // Store it
                 intersectedPoints[p] = line;
 
                 // Calculate distances between interesected point and "start" (i.e. line and start)
                 double distance_p = start.GetDistance(p);
+
                 if (!ordered_min_to_max.ContainsKey(distance_p))
                     ordered_min_to_max.Add(distance_p, p);
             }
@@ -779,9 +799,9 @@ namespace MissionPlanner.Utilities
             if (key == double.MaxValue)
                 key = ordered_keys[ordered_keys.Count-1];
 
-            // return line
-            return intersectedPoints[ordered_min_to_max[key]];
+            var filteredlist = intersectedPoints.Where(a => a.Key.GetDistance(start) >= key);
 
+            return findClosestLine(start, filteredlist.Select(a => a.Value).ToList(), 0, angle);
         }
 
         static bool PointInPolygon(utmpos p, List<utmpos> poly)
