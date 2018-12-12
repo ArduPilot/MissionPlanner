0fd32bd5c74b074c41cbe9d1af02f026051f1599
Michael Oborne <mich146@hotmail.com>
2018-12-13 07:15:16 +0800

    CsAssortedWidgets: netstandard

---
+++ ExtLibs/CsAssortedWidgets/Widgets/Cursors.cs
@@ -0,0 +1,13 @@
+﻿namespace System.Windows.Forms
+{
+    internal class Cursors
+    {
+        public static Cursor SizeWE { get; set; }
+        public static Cursor Default { get; set; }
+        public static Cursor Hand { get; set; }
+        public static Cursor SizeAll { get; set; }
+        public static Cursor SizeNS { get; set; }
+        public static Cursor SizeNWSE { get; set; }
+        public static Cursor SizeNESW { get; set; }
+    }
+}
