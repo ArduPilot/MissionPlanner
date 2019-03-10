7765e011eefc56cac7ad89a4e905cf6fea698090
Michael Oborne <mich146@hotmail.com>
2019-03-10 17:22:53 +0800

    ConfigUAVCAN

---
+++ GCSViews/ConfigurationView/ConfigUAVCAN.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace MissionPlanner.GCSViews.ConfigurationView
+{
+    partial class ConfigUAVCAN
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.but_slcanmode = new MissionPlanner.Controls.MyButton();
+            this.myDataGridView1 = new MissionPlanner.Controls.MyDataGridView();
+            this.uAVCANModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.healthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uptimeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hardwareVersionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.updateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.myDataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uAVCANModelBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label6.Location = new System.Drawing.Point(3, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 20);
+            this.label6.TabIndex = 80;
+            this.label6.Text = "UAVCAN";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox5.Location = new System.Drawing.Point(-1, 18);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(793, 5);
+            this.groupBox5.TabIndex = 79;
+            this.groupBox5.TabStop = false;
+            // 
+            // but_slcanmode
+            // 
+            this.but_slcanmode.Location = new System.Drawing.Point(7, 29);
+            this.but_slcanmode.Name = "but_slcanmode";
+            this.but_slcanmode.Size = new System.Drawing.Size(75, 23);
+            this.but_slcanmode.TabIndex = 82;
+            this.but_slcanmode.Text = "SLCan Mode";
+            this.but_slcanmode.UseVisualStyleBackColor = true;
+            this.but_slcanmode.Click += new System.EventHandler(this.but_slcanmode_Click);
+            // 
+            // myDataGridView1
+            // 
+            this.myDataGridView1.AllowUserToAddRows = false;
+            this.myDataGridView1.AllowUserToDeleteRows = false;
+            this.myDataGridView1.AllowUserToOrderColumns = true;
+            this.myDataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myDataGridView1.AutoGenerateColumns = false;
+            this.myDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myDataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.modeDataGridViewTextBoxColumn,
+            this.healthDataGridViewTextBoxColumn,
+            this.uptimeDataGridViewTextBoxColumn,
+            this.hardwareVersionDataGridViewTextBoxColumn,
+            this.updateDataGridViewTextBoxColumn});
+            this.myDataGridView1.DataSource = this.uAVCANModelBindingSource;
+            this.myDataGridView1.Location = new System.Drawing.Point(7, 58);
+            this.myDataGridView1.Name = "myDataGridView1";
+            this.myDataGridView1.ReadOnly = true;
+            this.myDataGridView1.Size = new System.Drawing.Size(785, 320);
+            this.myDataGridView1.TabIndex = 1;
+            this.myDataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.myDataGridView1_CellClick);
+            this.myDataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.myDataGridView1_RowsAdded);
+            // 
+            // uAVCANModelBindingSource
+            // 
+            this.uAVCANModelBindingSource.DataSource = typeof(MissionPlanner.GCSViews.ConfigurationView.UAVCANModel);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 26);
+            this.label1.TabIndex = 83;
+            this.label1.Text = "After enabling SLCAN, you will no longer be able to connect via MAVLINK.\r\nYou mus" +
+    "t reboot the flight controller to return to a normal mode\r\n";
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn.Width = 43;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 60;
+            // 
+            // modeDataGridViewTextBoxColumn
+            // 
+            this.modeDataGridViewTextBoxColumn.DataPropertyName = "Mode";
+            this.modeDataGridViewTextBoxColumn.HeaderText = "Mode";
+            this.modeDataGridViewTextBoxColumn.Name = "modeDataGridViewTextBoxColumn";
+            this.modeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // healthDataGridViewTextBoxColumn
+            // 
+            this.healthDataGridViewTextBoxColumn.DataPropertyName = "Health";
+            this.healthDataGridViewTextBoxColumn.HeaderText = "Health";
+            this.healthDataGridViewTextBoxColumn.Name = "healthDataGridViewTextBoxColumn";
+            this.healthDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // uptimeDataGridViewTextBoxColumn
+            // 
+            this.uptimeDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.uptimeDataGridViewTextBoxColumn.DataPropertyName = "Uptime";
+            this.uptimeDataGridViewTextBoxColumn.HeaderText = "Uptime";
+            this.uptimeDataGridViewTextBoxColumn.Name = "uptimeDataGridViewTextBoxColumn";
+            this.uptimeDataGridViewTextBoxColumn.ReadOnly = true;
+            this.uptimeDataGridViewTextBoxColumn.Width = 65;
+            // 
+            // hardwareVersionDataGridViewTextBoxColumn
+            // 
+            this.hardwareVersionDataGridViewTextBoxColumn.DataPropertyName = "HardwareVersion";
+            this.hardwareVersionDataGridViewTextBoxColumn.HeaderText = "HardwareVersion";
+            this.hardwareVersionDataGridViewTextBoxColumn.Name = "hardwareVersionDataGridViewTextBoxColumn";
+            this.hardwareVersionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // updateDataGridViewTextBoxColumn
+            // 
+            this.updateDataGridViewTextBoxColumn.HeaderText = "Update Firmware";
+            this.updateDataGridViewTextBoxColumn.Name = "updateDataGridViewTextBoxColumn";
+            this.updateDataGridViewTextBoxColumn.ReadOnly = true;
+            this.updateDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.updateDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.updateDataGridViewTextBoxColumn.Text = "Update Firmware";
+            // 
+            // ConfigUAVCAN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.but_slcanmode);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.myDataGridView1);
+            this.Name = "ConfigUAVCAN";
+            this.Size = new System.Drawing.Size(795, 381);
+            ((System.ComponentModel.ISupportInitialize)(this.myDataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uAVCANModelBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.MyDataGridView myDataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox5;
+        public System.Windows.Forms.BindingSource uAVCANModelBindingSource;
+        private Controls.MyButton but_slcanmode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn healthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uptimeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hardwareVersionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn updateDataGridViewTextBoxColumn;
+    }
+}
---
+++ GCSViews/ConfigurationView/ConfigUAVCAN.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UAVCAN;
+
+namespace MissionPlanner.GCSViews.ConfigurationView
+{
+    public partial class ConfigUAVCAN : UserControl, MissionPlanner.Controls.IDeactivate
+    {
+        public ConfigUAVCAN()
+        {
+            InitializeComponent();
+
+            uAVCANModelBindingSource.DataSource = allnodes;
+        }
+
+        private void configUAVCANBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        List<UAVCANModel> allnodes = new List<UAVCANModel>();
+
+        private void but_slcanmode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MainV2.comPort.setParam("CAN_SLCAN_SERNUM", 0, true);
+            } catch
+            {
+
+            }
+                    {
+                // fail is good
+
+                var port = MainV2.comPort.BaseStream;
+
+                MainV2.comPort.BaseStream = new Comms.SerialPort() { PortName = port.PortName, BaudRate = port.BaudRate };
+
+                can.SourceNode = 127;
+
+                can.NodeAdded += (id, msg) =>
+                {
+                    this.BeginInvoke((Action)delegate
+                    {
+                        allnodes.Add(new UAVCANModel()
+                        {
+                            ID = id,
+                            Name = "?",
+                            Health = msg.health.ToString(),
+                            Mode = msg.mode.ToString(),
+                            Uptime = TimeSpan.FromSeconds(msg.uptime_sec)
+                        });
+                    });
+                };
+
+                if(!port.IsOpen)
+                    port.Open();
+
+                can.StartSLCAN(port.BaseStream);
+
+                can.SetupFileServer();
+
+                can.SetupDynamicNodeAllocator();
+
+                can.MessageReceived += (frame, msg, transferID) =>
+                {
+                    if (msg.GetType() == typeof(UAVCAN.uavcan.uavcan_protocol_NodeStatus))
+                    {
+                        var ns = msg as UAVCAN.uavcan.uavcan_protocol_NodeStatus;
+
+                        var nodes = allnodes.Where((a) => a.ID == frame.SourceNode);
+
+                        if (nodes.Count() > 0 && nodes.First().Name == "?")
+                        {
+                            var statetracking = new UAVCAN.UAVCAN.statetracking();
+                            // get node info
+                            UAVCAN.uavcan.uavcan_protocol_GetNodeInfo_req gnireq = new UAVCAN.uavcan.uavcan_protocol_GetNodeInfo_req() { };
+                            gnireq.encode(UAVCAN.UAVCAN.chunk_cb, statetracking);
+
+                            var slcan = can.PackageMessage(frame.SourceNode, 30, 0, gnireq);
+                            can.WriteToStream(slcan);
+                        }
+
+                        foreach (var item in nodes)
+                        {
+                            item.Health = ns.health.ToString();
+                            item.Mode = ns.mode.ToString();
+                            item.Uptime = TimeSpan.FromSeconds(ns.uptime_sec);
+                        }
+
+                        this.BeginInvoke((Action)delegate
+                        {
+                            uAVCANModelBindingSource.ResetBindings(false);
+                        });
+                    }
+                    else if (msg.GetType() == typeof(UAVCAN.uavcan.uavcan_protocol_GetNodeInfo_res))
+                    {
+                        var gnires = msg as UAVCAN.uavcan.uavcan_protocol_GetNodeInfo_res;
+
+                        var nodes = allnodes.Where((a) => a.ID == frame.SourceNode);
+
+                        foreach (var item in nodes)
+                        {
+                            item.Name = ASCIIEncoding.ASCII.GetString(gnires.name, 0, gnires.name_len);
+                            item.HardwareVersion = gnires.hardware_version.major + "." + gnires.hardware_version.minor;
+                        }
+
+                        this.BeginInvoke((Action)delegate
+                        {
+                            uAVCANModelBindingSource.ResetBindings(false);
+                        });
+                    }
+                };
+            }            
+        }
+
+        UAVCAN.UAVCAN can = new UAVCAN.UAVCAN();
+
+        private void myDataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Ignore clicks that are not on button cells. 
+            if (e.RowIndex < 0 || e.ColumnIndex !=
+                myDataGridView1.Columns["updateDataGridViewTextBoxColumn"].Index) return;
+
+            byte nodeID = (byte)myDataGridView1[iDDataGridViewTextBoxColumn.Index, e.RowIndex].Value;
+            FileDialog fd = new OpenFileDialog();
+            fd.RestoreDirectory = true;
+            fd.Filter = "*-crc.bin|*-crc.bin";
+            var dia = fd.ShowDialog();
+
+            if(fd.CheckFileExists && dia == DialogResult.OK)
+                can.Update(myDataGridView1[nameDataGridViewTextBoxColumn.Index, e.RowIndex].Value.ToString(), 0, fd.FileName);
+        }
+
+        public void Deactivate()
+        {
+            can.Stop();
+            can = null;
+        }
+
+        private void myDataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            myDataGridView1[updateDataGridViewTextBoxColumn.Index, e.RowIndex].Value = "Update";
+        }
+    }
+
+    public class UAVCANModel
+    {
+        public byte ID { get; set; }
+        public string Name { get; set; } = "?";
+        public string Mode { get; set; }
+        public string Health { get; set; }
+        public TimeSpan Uptime { get; set; }
+        public string HardwareVersion { get; set; }
+    }
+}
