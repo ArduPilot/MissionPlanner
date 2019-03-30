dc821651c1eff0de9aba1fbed2e06a9994f06f9c
Michael Oborne <mich146@hotmail.com>
2019-03-30 11:00:18 +0800

    rtcm3: update for week rollover

--- ExtLibs/Utilities/rtcm3.cs
+++ ExtLibs/Utilities/rtcm3.cs
@@ -246,8 +246,12 @@ namespace MissionPlanner.Utilities
                             var tp = new type1019();
 
                             tp.Read(packet);
+                            
+                            var week = (int)tp.week;
+                            var tow = 0.0;
 
-                            var week = (int)tp.week + 1024;
+                            // assumes this is always a live stream
+                            StaticUtils.GetFromTime(DateTime.UtcNow, ref week, ref tow);
 
                             // both at start of a week
                             if (tp.toes < (60 * 60 * 24) && lasttow < (60 * 60 * 24))
@@ -2553,8 +2557,8 @@ namespace MissionPlanner.Utilities
                 const double NAV_OMEGAE_DOT = 7.2921151467e-005;
                 const double NAV_GM = 3.986005e14;
 
-                var toc = new gps_time_t(this.toc, (u16)(week+1024 ));
-                var toe = new gps_time_t(toes, (u16)(week+1024));
+                var toc = new gps_time_t(this.toc, (u16)(week));
+                var toe = new gps_time_t(toes, (u16)(week));
 
                 // Satellite clock terms
                 // Seconds from clock data reference time (toc)
