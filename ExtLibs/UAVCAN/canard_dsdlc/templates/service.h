81d26c88464eb2808a5183cb9e64106a0f5e716b
Michael Oborne <mich146@hotmail.com>
2019-04-12 13:00:20 +0800

    UAVCAN: add service to namespace

--- ExtLibs/UAVCAN/canard_dsdlc/templates/service.h
+++ ExtLibs/UAVCAN/canard_dsdlc/templates/service.h
@@ -1,8 +1,11 @@
 @[if msg.kind == msg.KIND_SERVICE]@
 @{from canard_dsdlc_helpers import *}@
-public partial class uavcan {
+namespace UAVCAN
+{
+	public partial class uavcan {
 
-const double @(underscored_name(msg).upper())_DT_ID = @(msg.default_dtid);
-const double @(underscored_name(msg).upper())_DT_SIG = @('0x%08X' % (msg.get_data_type_signature(),));
+		const double @(underscored_name(msg).upper())_DT_ID = @(msg.default_dtid);
+		const double @(underscored_name(msg).upper())_DT_SIG = @('0x%08X' % (msg.get_data_type_signature(), ));
+	}
 }
 @[end if]@
