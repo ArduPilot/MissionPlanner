70b0de3ad2e6462ef087f6de93a5c4dd4f50e1bf
Michael Oborne <mich146@hotmail.com>
2019-04-16 21:33:26 +0800

    drivers: update msi

--- Msi/drivers.bat
+++ Msi/drivers.bat
@@ -1,4 +1,4 @@
 "%wix%\bin\candle" drivers.wxs -ext WiXNetFxExtension -ext WixDifxAppExtension -ext WixUIExtension.dll -ext WixUtilExtension -ext WixIisExtension
 pause
-"%wix%\bin\light" drivers.wixobj "%wix%\bin\difxapp_x86.wixlib" -o px4driver.msi -ext WiXNetFxExtension -ext WixDifxAppExtension -ext WixUIExtension.dll -ext WixUtilExtension -ext WixIisExtension
+"%wix%\bin\light" drivers.wixobj "%wix%\bin\difxapp_x86.wixlib" -o driver.msi -ext WiXNetFxExtension -ext WixDifxAppExtension -ext WixUIExtension.dll -ext WixUtilExtension -ext WixIisExtension
 pause
