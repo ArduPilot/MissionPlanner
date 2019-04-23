b052e0acb6137e3865f2e3df9bc8607c961493e4
Michael Oborne <mich146@hotmail.com>
2019-04-23 15:32:21 +0800

    MissionPlanner 1.3.65

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.3.*")]
-[assembly: AssemblyFileVersion("1.3.64.2")]
+[assembly: AssemblyFileVersion("1.3.65")]
