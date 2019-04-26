6b48d862e33b42e81cafd0a2dd39f498f67c7e92
Michael Oborne <mich146@hotmail.com>
2019-04-26 16:37:46 +0800

    Drivers: update name

--- Drivers/inf2cat.bat
+++ Drivers/inf2cat.bat
@@ -1,10 +1,13 @@
 set PATH=%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;C:\Program Files (x86)\Windows Kits\10\bin\x86
 
-for /r %%v in (*.inf) do mkdir "%%v-t"
+for %%v in (*.inf) do (
+mkdir "%%v-t"
+copy "%%v" "%%v-t\"
+inf2cat.exe /driver:"%%v-t" /os:XP_X86,XP_X64,Server2003_X86,Server2003_X64,Vista_X86,Server2008_X86,Vista_X64,Server2008_X64,7_X86,7_X64,Server2008R2_X64,8_X86,8_X64,6_3_X86,6_3_X64,Server8_X64,10_X86,10_X64,Server10_X64
 
-for /r %%v in (*.inf) do copy "%%v" "%%v-t\"
+)
 
-for /F "eol=;" %%v in ('dir /b /ad *.inf') do inf2cat.exe /driver:"%%v" /os:XP_X86,XP_X64,Server2003_X86,Server2003_X64,Vista_X86,Server2008_X86,Vista_X64,Server2008_X64,7_X86,7_X64,Server2008R2_X64,8_X86,8_X64,6_3_X86,6_3_X64,Server8_X64,10_X86,10_X64,Server10_X64
+pause
 
 for /r %%v in (*.cat) do copy "%%v" ".\"
 
