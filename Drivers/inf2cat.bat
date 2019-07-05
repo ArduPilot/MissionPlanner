077a2fdaca1a009d337d6c9e7a48f28a0e6290c8
Michael Oborne <mich146@hotmail.com>
2019-07-05 16:09:30 +0800

    Drivers: update cube yellow

--- Drivers/inf2cat.bat
+++ Drivers/inf2cat.bat
@@ -1,4 +1,4 @@
-set PATH=%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;C:\Program Files (x86)\Windows Kits\10\bin\x86
+set PATH=%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;C:\Program Files (x86)\Windows Kits\10\bin\x86;C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64
 
 for %%v in (*.inf) do (
 mkdir "%%v-t"
