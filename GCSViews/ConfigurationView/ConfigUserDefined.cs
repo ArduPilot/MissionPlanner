65d69ea417535e77dcbacc15981c27ad7c23ae3c
Michael Oborne <mich146@hotmail.com>
2019-07-22 09:20:26 +0800

    ConfigUserDefined: add user modify

--- GCSViews/ConfigurationView/ConfigUserDefined.cs
+++ GCSViews/ConfigurationView/ConfigUserDefined.cs
@@ -32,7 +32,17 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             "CH11_OPT",
             "CH12_OPT",
             "CH13_OPT",
-            "CH14_OPT"
+            "CH14_OPT",
+
+            "RC6_OPTION",
+            "RC7_OPTION",
+            "RC8_OPTION",
+            "RC9_OPTION",
+            "RC10_OPTION",
+            "RC11_OPTION",
+            "RC12_OPTION",
+            "RC13_OPTION",
+            "RC14_OPTION"
         };
 
         public void LoadOptions()
@@ -41,8 +51,22 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             tableLayoutPanel1.Controls.Clear();
             tableLayoutPanel1.RowCount = 0;
 
+            var button = new MyButton() { Text = "Modify", Name = "Modify" };
+            button.Click += (o, e) =>
+            {
+                var opts = Options.Aggregate((a, b) => a + "\r\n" + b);
+                InputBox.Show("Params", "Enter Param Names", ref opts, false, true);
+                Options = opts.Split(new[] {',','\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                Settings.Instance["UserParams"] = Options.Aggregate((a, b) => a.Trim() + "," + b.Trim());
+            };
+            tableLayoutPanel1.RowCount++;
+            tableLayoutPanel1.Controls.Add(button);
+            tableLayoutPanel1.SetColumnSpan(button, 2);
+
             foreach (var option in Options)
             {
+                if (MainV2.comPort.MAV.param.ContainsKey(option))
+                    continue;
                 tableLayoutPanel1.RowCount++;
                 tableLayoutPanel1.Controls.Add(new Label() {Text = option, Name = option});
                 var cmb = new MavlinkComboBox();
