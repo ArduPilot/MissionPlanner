341ef895eaa381fdf384d36187b799dcda35748e
Michael Oborne <mich146@hotmail.com>
2019-10-10 17:48:58 +0800

    build: update to vs2019

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 
-set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
+set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin;C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
 
 del bin\release\MissionPlannerBeta.zip
 
