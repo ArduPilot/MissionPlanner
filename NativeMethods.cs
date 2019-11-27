da46b2894288799fa0e018053795e5ef4548fec8
Michael Oborne <mich146@hotmail.com>
2019-11-27 13:45:35 +0800

    MainV2:  move nativemethods

---
+++ NativeMethods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MissionPlanner
+{
+    internal static class NativeMethods
+    {
+        // used to hide/show console window
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string szClass, string szTitle);
+
+        [DllImport("user32.dll")]
+        public static extern int ShowWindow(int Handle, int showState);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern IntPtr RegisterDeviceNotification
+        (IntPtr hRecipient,
+            IntPtr NotificationFilter,
+            Int32 Flags);
+
+        // Import SetThreadExecutionState Win32 API and necessary flags
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SetThreadExecutionState(uint esFlags);
+
+        public const uint ES_CONTINUOUS = 0x80000000;
+        public const uint ES_SYSTEM_REQUIRED = 0x00000001;
+
+        static public int SW_SHOWNORMAL = 1;
+        static public int SW_HIDE = 0;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
+
+        [System.Flags]
+        public enum LoadLibraryFlags : uint
+        {
+            DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+            LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
+            LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
+            LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
+            LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
+            LOAD_LIBRARY_SEARCH_APPLICATION_DIR = 0x00000200,
+            LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000,
+            LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100,
+            LOAD_LIBRARY_SEARCH_SYSTEM32 = 0x00000800,
+            LOAD_LIBRARY_SEARCH_USER_DIRS = 0x00000400,
+            LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008
+        }
+    }
+}
