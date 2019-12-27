f814a7d6b14d8d0e8d0f27600b552de6e4a5df19
Michael Oborne <mich146@hotmail.com>
2019-12-28 07:26:47 +0800

    Controls: TransparentPanel

---
+++ ExtLibs/Controls/TransparentPanel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner.Controls
+{
+    public class TransparentPanel : Panel
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
+                return cp;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(this.BackColor), this.ClientRectangle);
+        }
+    }
+}
