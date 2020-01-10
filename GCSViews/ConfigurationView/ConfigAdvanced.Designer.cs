5b7b0469b4988c135de0bd8a7425d9ecbddcf285
Michael Oborne <mich146@hotmail.com>
2020-01-11 06:29:59 +0800

    ConfigAdvanced: add more buttons

--- GCSViews/ConfigurationView/ConfigAdvanced.Designer.cs
+++ GCSViews/ConfigurationView/ConfigAdvanced.Designer.cs
@@ -36,6 +36,22 @@
             this.but_signkey = new MissionPlanner.Controls.MyButton();
             this.BUT_outputMavlink = new MissionPlanner.Controls.MyButton();
             this.BUT_outputnmea = new MissionPlanner.Controls.MyButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BUT_follow_me = new MissionPlanner.Controls.MyButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.BUT_paramgen = new MissionPlanner.Controls.MyButton();
+            this.label9 = new System.Windows.Forms.Label();
+            this.BUT_movingbase = new MissionPlanner.Controls.MyButton();
+            this.label10 = new System.Windows.Forms.Label();
+            this.but_anonlog = new MissionPlanner.Controls.MyButton();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -85,17 +101,117 @@
             this.BUT_outputnmea.UseVisualStyleBackColor = true;
             this.BUT_outputnmea.Click += new System.EventHandler(this.BUT_outputnmea_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.label7, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.but_warningmanager, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.BUT_outputnmea, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.but_mavinspector, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.BUT_outputMavlink, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.but_proximity, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.but_signkey, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label11, 1, 9);
+            this.tableLayoutPanel1.Controls.Add(this.BUT_follow_me, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.BUT_paramgen, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 1, 8);
+            this.tableLayoutPanel1.Controls.Add(this.BUT_movingbase, 0, 8);
+            this.tableLayoutPanel1.Controls.Add(this.but_anonlog, 0, 9);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // BUT_follow_me
+            // 
+            resources.ApplyResources(this.BUT_follow_me, "BUT_follow_me");
+            this.BUT_follow_me.Name = "BUT_follow_me";
+            this.BUT_follow_me.UseVisualStyleBackColor = true;
+            this.BUT_follow_me.Click += new System.EventHandler(this.BUT_follow_me_Click);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // BUT_paramgen
+            // 
+            resources.ApplyResources(this.BUT_paramgen, "BUT_paramgen");
+            this.BUT_paramgen.Name = "BUT_paramgen";
+            this.BUT_paramgen.UseVisualStyleBackColor = true;
+            this.BUT_paramgen.Click += new System.EventHandler(this.BUT_paramgen_Click);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // BUT_movingbase
+            // 
+            resources.ApplyResources(this.BUT_movingbase, "BUT_movingbase");
+            this.BUT_movingbase.Name = "BUT_movingbase";
+            this.BUT_movingbase.UseVisualStyleBackColor = true;
+            this.BUT_movingbase.Click += new System.EventHandler(this.BUT_movingbase_Click);
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // but_anonlog
+            // 
+            resources.ApplyResources(this.but_anonlog, "but_anonlog");
+            this.but_anonlog.Name = "but_anonlog";
+            this.but_anonlog.UseVisualStyleBackColor = true;
+            this.but_anonlog.Click += new System.EventHandler(this.but_anonlog_Click);
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
             // ConfigAdvanced
             // 
-            this.Controls.Add(this.BUT_outputnmea);
-            this.Controls.Add(this.BUT_outputMavlink);
-            this.Controls.Add(this.but_signkey);
-            this.Controls.Add(this.but_proximity);
-            this.Controls.Add(this.but_warningmanager);
-            this.Controls.Add(this.but_mavinspector);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label1);
             this.Name = "ConfigAdvanced";
             resources.ApplyResources(this, "$this");
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,5 +226,20 @@
         private Controls.MyButton but_signkey;
         private Controls.MyButton BUT_outputMavlink;
         private Controls.MyButton BUT_outputnmea;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Controls.MyButton BUT_follow_me;
+        private System.Windows.Forms.Label label8;
+        private Controls.MyButton BUT_paramgen;
+        private System.Windows.Forms.Label label9;
+        private Controls.MyButton BUT_movingbase;
+        private System.Windows.Forms.Label label10;
+        private Controls.MyButton but_anonlog;
+        private System.Windows.Forms.Label label11;
     }
 }
--- GCSViews/ConfigurationView/ConfigAdvanced.cs
+++ GCSViews/ConfigurationView/ConfigAdvanced.cs
@@ -1,5 +1,9 @@
-﻿using MissionPlanner.Controls;
+﻿using System;
+using System.Configuration;
+using System.IO;
+using MissionPlanner.Controls;
 using System.Windows.Forms;
+using MissionPlanner.Utilities;
 using MissionPlanner.Warnings;
 
 namespace MissionPlanner.GCSViews.ConfigurationView
@@ -43,6 +47,48 @@ namespace MissionPlanner.GCSViews.ConfigurationView
         private void BUT_outputnmea_Click(object sender, System.EventArgs e)
         {
             new SerialOutputNMEA().Show();
+        }
+
+        private void BUT_follow_me_Click(object sender, System.EventArgs e)
+        {
+            new FollowMe().Show();
+        }
+
+        private void BUT_paramgen_Click(object sender, System.EventArgs e)
+        {
+            ParameterMetaDataParser.GetParameterInformation(ConfigurationManager.AppSettings["ParameterLocationsBleeding"] + ";" + ConfigurationManager.AppSettings["ParameterLocations"]);
+
+            ParameterMetaDataRepositoryAPM.Reload();
+        }
+
+        private void BUT_movingbase_Click(object sender, System.EventArgs e)
+        {
+            new MovingBase().Show();
+        }
+
+        private void but_anonlog_Click(object sender, System.EventArgs e)
+        {
+            CustomMessageBox.Show("This is beta, please confirm the output file");
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "tlog or bin/log|*.tlog;*.bin;*.log";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    var ext = Path.GetExtension(ofd.FileName).ToLower();
+                    if (ext == ".bin")
+                        ext = ".log";
+                    using (SaveFileDialog sfd = new SaveFileDialog())
+                    {
+                        sfd.DefaultExt = ext;
+                        sfd.FileName = Path.GetFileNameWithoutExtension(ofd.FileName) + "-anon" + ext;
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                        {
+                            Privacy.anonymise(ofd.FileName, sfd.FileName);
+                        }
+                    }
+                }
+            }
         }
     }
 }
