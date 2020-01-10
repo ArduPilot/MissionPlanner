672fd8e30a546d96cd9178dae00800592ca6496f
Michael Oborne <mich146@hotmail.com>
2020-01-10 21:14:24 +0800

    ConfigAdvanced: make buttons work

--- GCSViews/ConfigurationView/ConfigAdvanced.Designer.cs
+++ GCSViews/ConfigurationView/ConfigAdvanced.Designer.cs
@@ -31,7 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigAdvanced));
             this.label1 = new System.Windows.Forms.Label();
             this.but_mavinspector = new MissionPlanner.Controls.MyButton();
-            this.button3 = new MissionPlanner.Controls.MyButton();
+            this.but_warningmanager = new MissionPlanner.Controls.MyButton();
+            this.but_proximity = new MissionPlanner.Controls.MyButton();
+            this.but_signkey = new MissionPlanner.Controls.MyButton();
+            this.BUT_outputMavlink = new MissionPlanner.Controls.MyButton();
+            this.BUT_outputnmea = new MissionPlanner.Controls.MyButton();
             this.SuspendLayout();
             // 
             // label1
@@ -44,16 +48,50 @@
             resources.ApplyResources(this.but_mavinspector, "but_mavinspector");
             this.but_mavinspector.Name = "but_mavinspector";
             this.but_mavinspector.UseVisualStyleBackColor = true;
+            this.but_mavinspector.Click += new System.EventHandler(this.but_mavinspector_Click);
             // 
-            // button3
+            // but_warningmanager
             // 
-            resources.ApplyResources(this.button3, "button3");
-            this.button3.Name = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.but_warningmanager, "but_warningmanager");
+            this.but_warningmanager.Name = "but_warningmanager";
+            this.but_warningmanager.UseVisualStyleBackColor = true;
+            this.but_warningmanager.Click += new System.EventHandler(this.but_warningmanager_Click);
+            // 
+            // but_proximity
+            // 
+            resources.ApplyResources(this.but_proximity, "but_proximity");
+            this.but_proximity.Name = "but_proximity";
+            this.but_proximity.UseVisualStyleBackColor = true;
+            this.but_proximity.Click += new System.EventHandler(this.but_proximity_Click);
+            // 
+            // but_signkey
+            // 
+            resources.ApplyResources(this.but_signkey, "but_signkey");
+            this.but_signkey.Name = "but_signkey";
+            this.but_signkey.UseVisualStyleBackColor = true;
+            this.but_signkey.Click += new System.EventHandler(this.but_signkey_Click);
+            // 
+            // BUT_outputMavlink
+            // 
+            resources.ApplyResources(this.BUT_outputMavlink, "BUT_outputMavlink");
+            this.BUT_outputMavlink.Name = "BUT_outputMavlink";
+            this.BUT_outputMavlink.UseVisualStyleBackColor = true;
+            this.BUT_outputMavlink.Click += new System.EventHandler(this.BUT_outputMavlink_Click);
+            // 
+            // BUT_outputnmea
+            // 
+            resources.ApplyResources(this.BUT_outputnmea, "BUT_outputnmea");
+            this.BUT_outputnmea.Name = "BUT_outputnmea";
+            this.BUT_outputnmea.UseVisualStyleBackColor = true;
+            this.BUT_outputnmea.Click += new System.EventHandler(this.BUT_outputnmea_Click);
             // 
             // ConfigAdvanced
             // 
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.BUT_outputnmea);
+            this.Controls.Add(this.BUT_outputMavlink);
+            this.Controls.Add(this.but_signkey);
+            this.Controls.Add(this.but_proximity);
+            this.Controls.Add(this.but_warningmanager);
             this.Controls.Add(this.but_mavinspector);
             this.Controls.Add(this.label1);
             this.Name = "ConfigAdvanced";
@@ -67,6 +105,10 @@
 
         private System.Windows.Forms.Label label1;
         private Controls.MyButton but_mavinspector;
-        private Controls.MyButton button3;
+        private Controls.MyButton but_warningmanager;
+        private Controls.MyButton but_proximity;
+        private Controls.MyButton but_signkey;
+        private Controls.MyButton BUT_outputMavlink;
+        private Controls.MyButton BUT_outputnmea;
     }
 }
--- GCSViews/ConfigurationView/ConfigAdvanced.cs
+++ GCSViews/ConfigurationView/ConfigAdvanced.cs
@@ -1,5 +1,6 @@
 ﻿using MissionPlanner.Controls;
 using System.Windows.Forms;
+using MissionPlanner.Warnings;
 
 namespace MissionPlanner.GCSViews.ConfigurationView
 {
@@ -12,6 +13,36 @@ namespace MissionPlanner.GCSViews.ConfigurationView
 
         public void Activate()
         {
+        }
+
+        private void but_warningmanager_Click(object sender, System.EventArgs e)
+        {
+            new WarningsManager().Show();
+        }
+
+        private void but_mavinspector_Click(object sender, System.EventArgs e)
+        {
+            new MAVLinkInspector(MainV2.comPort).Show();
+        }
+
+        private void BUT_outputMavlink_Click(object sender, System.EventArgs e)
+        {
+            new SerialOutputPass().Show();
+        }
+
+        private void but_signkey_Click(object sender, System.EventArgs e)
+        {
+            new AuthKeys().Show();
+        }
+
+        private void but_proximity_Click(object sender, System.EventArgs e)
+        {
+            new ProximityControl(MainV2.comPort.MAV).Show();
+        }
+
+        private void BUT_outputnmea_Click(object sender, System.EventArgs e)
+        {
+            new SerialOutputNMEA().Show();
         }
     }
 }
