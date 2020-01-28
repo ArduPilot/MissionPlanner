8c4e0ebbbe9f20f385f87800b35616f2b9ed86d2
Michael Oborne <mich146@hotmail.com>
2020-01-29 07:08:19 +0800

    wasm: add websocket helper

---
+++ ExtLibs/wasm/BlazorWebSocketHelper-master/Classes/BwsError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebSocketHelper.Classes
+{
+    public class BwsError
+    {
+        public string Message { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ExtLibs/wasm/BlazorWebSocketHelper-master/Classes/BwsMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static BlazorWebSocketHelper.Classes.BwsEnums;
+
+namespace BlazorWebSocketHelper.Classes
+{
+    public class BwsMessage
+    {
+        public int ID { get; set; }
+        public Guid GUID { get; set; }  = Guid.NewGuid();
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
+        public byte[] MessageBinary { get; set; }
+        public string MessageBinaryVisual { get; set; }
+        public BwsMessageType MessageType { get; set; }
+        public BwsTransportType TransportType { get; set; }
+    }
+}
