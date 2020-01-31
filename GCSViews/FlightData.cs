1bdcba651aedce1f7b25a10c1c7433a559e252bd
Michael Oborne <mich146@hotmail.com>
2020-01-31 17:51:02 +0800

    FlightData: fix quickview total check

--- GCSViews/FlightData.cs
+++ GCSViews/FlightData.cs
@@ -3823,7 +3823,7 @@ namespace MissionPlanner.GCSViews
             }).ToList();
 
             // add extra
-            while (total < tableLayoutPanelQuick.Controls.Count)
+            while (total > tableLayoutPanelQuick.Controls.Count)
             {
                 var QV = new QuickView()
                 {
