f3703fede15cab8c1b7c78b7effc238cafcd92e9
Michael Oborne <mich146@hotmail.com>
2020-02-15 18:43:09 +0800

    ConfigParamLoading: add

---
+++ GCSViews/ConfigurationView/ConfigParamLoading.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MissionPlanner.Controls;
+
+namespace MissionPlanner.GCSViews.ConfigurationView
+{
+    public partial class ConfigParamLoading : UserControl, IActivate, IDeactivate
+    {
+        public bool gotAllParams
+        {
+            get
+            {
+                if (MainV2.comPort.MAV.param.TotalReceived < MainV2.comPort.MAV.param.TotalReported)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        public ConfigParamLoading()
+        {
+            InitializeComponent();
+        }
+
+        public void Activate()
+        {
+            timer1.Start();
+        }
+
+        public void Deactivate()
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (gotAllParams)
+                MainV2.View.Reload();
+        }
+    }
+}
