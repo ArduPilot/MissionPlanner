ab88b301600ac3a1fe2be6571c6a2afaf23f3f1d
Andras Schaffer <eosbandi@gmail.com>
2020-02-24 11:58:24 +0100

    Sorting items in Status tab
    Item order in Status tab is quite random (created via evolution). Sorting them alphabetically makes it more searchable.
    If there are negative used feedback then it can be made a config option, but I don't see any drawback at this moment.

--- GCSViews/FlightData.cs
+++ GCSViews/FlightData.cs
@@ -4199,7 +4199,7 @@ namespace MissionPlanner.GCSViews
             int x = 10;
             int y = 10;
 
-            var list = MainV2.comPort.MAV.cs.GetItemList();
+            var list = MainV2.comPort.MAV.cs.GetItemList(true);
 
             tabStatus.SuspendLayout();
 
