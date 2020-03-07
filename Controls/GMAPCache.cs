61ecbefd4d70e538c48709546ac3fe2fe885b025
Michael Oborne <mich146@hotmail.com>
2020-03-08 05:52:16 +0800

    GMAPCache: add remove all

--- Controls/GMAPCache.cs
+++ Controls/GMAPCache.cs
@@ -19,7 +19,7 @@ namespace MissionPlanner.Controls
     public partial class GMAPCache : UserControl, IActivate
     {
         private DataGridViewButtonColumn buttonColumn;
-        bool _init = false;
+        private DataGridViewButtonColumn buttonColumn2;
 
         public GMAPCache()
         {
@@ -35,7 +35,9 @@ namespace MissionPlanner.Controls
             var sizecountoverall = DirSize(new DirectoryInfo(dir));
             var overall = new
             {
-                Name = "Total", Size = Math.Round(sizecountoverall.Item1 / 1024.0 / 1024.0, MidpointRounding.AwayFromZero) + "MB", Count = sizecountoverall.Item2
+                Name = "Total",
+                Size = Math.Round(sizecountoverall.Item1 / 1024.0 / 1024.0, MidpointRounding.AwayFromZero) + "MB",
+                Count = sizecountoverall.Item2
             };
 
             List<object> list = new List<object>();
@@ -45,41 +47,73 @@ namespace MissionPlanner.Controls
                 var sizecount = DirSize(new DirectoryInfo(s));
 
                 list.Add(new
-                    {Name = Path.GetFileName(s), Size = Math.Round(sizecount.Item1 / 1024.0 / 1024.0, MidpointRounding.AwayFromZero) + "MB", Count = sizecount.Item2});
+                {
+                    Name = Path.GetFileName(s),
+                    Size = Math.Round(sizecount.Item1 / 1024.0 / 1024.0, MidpointRounding.AwayFromZero) + "MB",
+                    Count = sizecount.Item2
+                });
             }
-            
+
             list.Add(overall);
+
+            myDataGridView1.Columns.Clear();
+            myDataGridView1.AutoGenerateColumns = true;
+
+            myDataGridView1.CellClick -=
+                new DataGridViewCellEventHandler(myDataGridView1_CellClick);
 
             var bs = new BindingSource();
             bs.DataSource = list;
             myDataGridView1.DataSource = bs;
 
-            if(!_init)
             {
                 // Add a button column. 
                 buttonColumn = new DataGridViewButtonColumn();
                 buttonColumn.HeaderText = "";
                 //buttonColumn.Name = "Status Request";
-                buttonColumn.Text = "Remove old";
+                buttonColumn.Text = "Remove 30+days";
                 buttonColumn.UseColumnTextForButtonValue = true;
 
                 myDataGridView1.Columns.Add(buttonColumn);
-
-                myDataGridView1.CellClick +=
-                    new DataGridViewCellEventHandler(myDataGridView1_CellClick);
-                _init = true;
             }
+
+            {
+                // Add a button column. 
+                buttonColumn2 = new DataGridViewButtonColumn();
+                buttonColumn2.HeaderText = "";
+                //buttonColumn.Name = "Status Request";
+                buttonColumn2.Text = "Remove All";
+                buttonColumn2.UseColumnTextForButtonValue = true;
+
+                myDataGridView1.Columns.Add(buttonColumn2);
+            }
+            myDataGridView1.CellClick +=
+                new DataGridViewCellEventHandler(myDataGridView1_CellClick);
+
         }
 
         private void myDataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             // Ignore clicks that are not on button cells. 
-            if (e.RowIndex < 0 || e.ColumnIndex != buttonColumn.Index) return;
+            if (e.RowIndex < 0) return;
 
-            string dir = myDataGridView1[0, e.RowIndex].Value as string;
-
+            if (e.ColumnIndex == buttonColumn.Index)
             {
+                string dir = myDataGridView1[0, e.RowIndex].Value as string;
+
                 var removed = ((PureImageCache) MyImageCache.Instance).DeleteOlderThan(DateTime.Now.AddDays(-30),
+                    GMapProviders.List.First(a => a.Name == dir).DbId);
+
+                CustomMessageBox.Show("Removed " + removed + " images");
+
+                Activate();
+            }
+
+            if (e.ColumnIndex == buttonColumn2.Index)
+            {
+                string dir = myDataGridView1[0, e.RowIndex].Value as string;
+
+                var removed = ((PureImageCache) MyImageCache.Instance).DeleteOlderThan(DateTime.Now.AddDays(0),
                     GMapProviders.List.First(a => a.Name == dir).DbId);
 
                 CustomMessageBox.Show("Removed " + removed + " images");
