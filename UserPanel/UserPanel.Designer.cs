ddb70f9d5754e899ec088ba40b71e9627217059f
Michael Oborne <mich146@hotmail.com>
2020-03-10 12:43:16 +0800

    UserPanel: template

---
+++ UserPanel/UserPanel.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace MissionPlanner.UserPanel
+{
+    partial class UserPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(583, 516);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // UserPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "UserPanel";
+            this.Size = new System.Drawing.Size(583, 516);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ UserPanel/UserPanel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner.UserPanel
+{
+    public partial class UserPanel : UserControl
+    {
+        public UserPanel()
+        {
+            InitializeComponent();
+        }
+
+        void test()
+        {
+            MainV2.comPort.doCommandAsync(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid, 0, 0, 0, 0, 0, 0, 0, 0);
+            MainV2.comPort.doARMAsync(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid, true);
+            MainV2.comPort.doCommandIntAsync(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid, 0, 0, 0, 0, 0, 0, 0, 0);
+            MainV2.comPort.getParamListAsync(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid);
+            MainV2.comPort.getHeartBeatAsync();
+            MainV2.comPort.getVersionAsync(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid);
+            MainV2.comPort.GetParamAsync(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid);
+        }
+    }
+}
