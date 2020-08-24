559cd0d95fe57e7141b65b102c207001d1817faa
Michael Oborne <mich146@hotmail.com>
2020-08-24 17:29:47 +0800

    OSDConfig: fix resource

--- ExtLibs/OSDConfigurator/Properties/Resources.Designer.cs
+++ ExtLibs/OSDConfigurator/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace OSDConfigurator.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MissionPlanner.ExtLibs.OSDConfigurator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OSDConfigurator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
