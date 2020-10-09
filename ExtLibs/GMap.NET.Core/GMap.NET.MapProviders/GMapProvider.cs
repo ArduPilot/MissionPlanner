60fa2b549aee236c3762a3bd22139c2d14176de6
Michael Oborne <mich146@hotmail.com>
2020-10-09 11:08:46 +0800

    GMapProviders: change download async type

--- ExtLibs/GMap.NET.Core/GMap.NET.MapProviders/GMapProvider.cs
+++ ExtLibs/GMap.NET.Core/GMap.NET.MapProviders/GMapProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace GMap.NET.MapProviders
 {
@@ -397,8 +398,10 @@ namespace GMap.NET.MapProviders
             if (!string.IsNullOrEmpty(RefererUrl))
                 client.DefaultRequestHeaders.Add("Referer", RefererUrl);
 
-            MemoryStream data =
-                Stuff.CopyStream(client.GetStreamAsync(url).ConfigureAwait(false).GetAwaiter().GetResult(), false);
+            MemoryStream data = Task.Run(async () =>
+            {
+                return Stuff.CopyStream(await client.GetStreamAsync(url), false);
+            }).Result;
 
             Debug.WriteLine("Response[" + data.Length + " bytes]: " + url);
 
@@ -424,67 +427,18 @@ namespace GMap.NET.MapProviders
         {
             string ret = string.Empty;
 
-#if !PocketPC
-            WebRequest request = IsSocksProxy ? SocksHttpWebRequest.Create(url) : WebRequest.Create(url);
-#else
-            WebRequest request = WebRequest.Create(url);
-#endif
+            HttpClient client = new HttpClient();
+            if (!string.IsNullOrEmpty(UserAgent))
+                client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+            if (!string.IsNullOrEmpty(requestAccept))
+                client.DefaultRequestHeaders.Add("Accept", requestAccept);
+            if (!string.IsNullOrEmpty(RefererUrl))
+                client.DefaultRequestHeaders.Add("Referer", RefererUrl);
 
-            if (WebProxy != null)
+            ret = Task.Run(async () =>
             {
-                request.Proxy = WebProxy;
-            }
-
-            if (Credential != null)
-            {
-                request.PreAuthenticate = true;
-                request.Credentials = Credential;
-            }
-
-            if (request is HttpWebRequest)
-            {
-                var r = request as HttpWebRequest;
-                r.UserAgent = UserAgent;
-                r.ReadWriteTimeout = TimeoutMs * 6;
-                r.Accept = requestAccept;
-                r.Referer = RefererUrl;
-                r.Timeout = TimeoutMs;
-            }
-#if !PocketPC
-            else if (request is SocksHttpWebRequest)
-            {
-                var r = request as SocksHttpWebRequest;
-
-                if (!string.IsNullOrEmpty(UserAgent))
-                {
-                    r.Headers.Add("User-Agent", UserAgent);
-                }
-
-                if (!string.IsNullOrEmpty(requestAccept))
-                {
-                    r.Headers.Add("Accept", requestAccept);
-                }
-
-                if (!string.IsNullOrEmpty(RefererUrl))
-                {
-                    r.Headers.Add("Referer", RefererUrl);
-                }
-            }
-#endif
-            using (var response = request.GetResponse())
-            {
-                using (Stream responseStream = response.GetResponseStream())
-                {
-                    using (StreamReader read = new StreamReader(responseStream, Encoding.UTF8))
-                    {
-                        ret = read.ReadToEnd();
-                    }
-                }
-#if PocketPC
-                request.Abort();
-#endif
-                response.Close();
-            }
+                return await client.GetStringAsync(url);
+            }).Result;
 
             return ret;
         }
