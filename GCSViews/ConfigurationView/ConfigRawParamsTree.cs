d8a7ec4e8ee561a1c6d4f4c28f91d04913485721
Andras Schaffer <eosbandi@gmail.com>
2021-03-10 00:36:17 +0100

    Update ConfigRawParamsTree
    Handle comma/period decimal separator issue.

--- GCSViews/ConfigurationView/ConfigRawParamsTree.cs
+++ GCSViews/ConfigurationView/ConfigRawParamsTree.cs
@@ -631,7 +631,7 @@ namespace MissionPlanner.GCSViews.ConfigurationView
 
                 try
                 {
-                    newvalue = float.Parse(e.NewValue.ToString());
+                    newvalue = float.Parse(e.NewValue.ToString().Replace(',','.'), CultureInfo.InvariantCulture);
                 }
                 catch
                 {
