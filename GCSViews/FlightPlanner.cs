70711fdc690f94cb0dba08ef02b376f40e30f5aa
murata <ma2maru@gmail.com>
2021-04-28 08:22:11 +0900

    GCSViews: Delete unused variables

--- GCSViews/FlightPlanner.cs
+++ GCSViews/FlightPlanner.cs
@@ -5405,8 +5405,6 @@ Column 1: Field type (RALLY is the only one at the moment -- may have RALLY_LAND
                     throw new Exception("Please connect first!");
                 }
 
-                int a = 0;
-
                 // define the home point
                 Locationwp home = new Locationwp();
                 try
@@ -6928,7 +6926,6 @@ Column 1: Field type (RALLY is the only one at the moment -- may have RALLY_LAND
                         var length = (int)list.First().p1;
                         if (length == 0)
                             length = 1;
-                        int cnt = 0;
                         var sublist = list.Take(length).ToList();
 
                         if (sublist.Contains(rowdelete))
