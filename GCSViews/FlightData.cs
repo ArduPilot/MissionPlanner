de67dd0e7b8358d50559c2eacad6155226c6d3ab
murata <ma2maru@gmail.com>
2021-05-03 18:46:55 +0900

    GCSViews: Set the WAY POINT radius to a floating value

--- GCSViews/FlightData.cs
+++ GCSViews/FlightData.cs
@@ -834,8 +834,8 @@ namespace MissionPlanner.GCSViews
                     mBorders.InnerMarker = m;
                     try
                     {
-                        mBorders.wprad =
-                            (int) (Settings.Instance.GetFloat("TXT_WPRad") / CurrentState.multiplierdist);
+                        mBorders.wprad = 
+                            (Settings.Instance.GetFloat("TXT_WPRad") / CurrentState.multiplierdist);
                     }
                     catch
                     {
