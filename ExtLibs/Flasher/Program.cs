d47a282ed3cee34c5845680be210def03c9a687e
Michael Oborne <mich146@hotmail.com>
2021-05-20 17:31:35 +1000

    Flasher: add

---
+++ ExtLibs/Flasher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+using MissionPlanner.Comms;
+
+namespace Flasher
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.Run(new FlasherForm(args));
+        }
+    }
+}
