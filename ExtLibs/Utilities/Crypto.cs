515d7f260edfaaf78da6f80f26560f83662f6bd2
Michael Oborne <mich146@hotmail.com>
2021-06-02 18:55:34 +1000

    Crypto: fix buffer size

--- ExtLibs/Utilities/Crypto.cs
+++ ExtLibs/Utilities/Crypto.cs
@@ -55,6 +55,7 @@ namespace MissionPlanner.Utilities
 
             this.algorithm = new RijndaelManaged();
             this.algorithm.Mode = CipherMode.CBC;
+            this.algorithm.Padding = PaddingMode.PKCS7;
             this.algorithm.Key = Key;
             this.algorithm.IV = IV;
         }
@@ -117,7 +118,7 @@ namespace MissionPlanner.Utilities
             cryptor.Dispose();
             cryptor = null;
 
-            return memoryStream.GetBuffer();
+            return memoryStream.ToArray();
         }
 
         /// <summary>
