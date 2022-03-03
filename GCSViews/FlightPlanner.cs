1dcf9161214d5581d472283570740b056b9db82e
AlexandreLarribau <72812521+AlexandreLarribau@users.noreply.github.com>
2022-03-03 09:43:57 +0100

    Revert "a supprimer"
    This reverts commit a1c4ece044889b19d101fb90bfaeabc213602fe7.

--- GCSViews/FlightPlanner.cs
+++ GCSViews/FlightPlanner.cs
@@ -7766,10 +7766,5 @@ Column 1: Field type (RALLY is the only one at the moment -- may have RALLY_LAND
                 CustomMessageBox.Show(Strings.ErrorCommunicating, Strings.ERROR);
             }
         }
-
-        private void grilleDeSurveillanceToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
