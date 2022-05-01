7166a330581cd87e96c268935314de71ed7d3179
Michael Oborne <mich146@hotmail.com>
2022-05-02 08:18:23 +1000

    Xamarin: fix osx compile

--- ExtLibs/Xamarin/Xamarin.MacOS/SystemInfo.cs
+++ ExtLibs/Xamarin/Xamarin.MacOS/SystemInfo.cs
@@ -1,10 +1,17 @@
-﻿namespace Xamarin.MacOS
+﻿using System.Linq;
+
+namespace Xamarin.MacOS
 {
     internal class SystemInfo : ISystemInfo
     {
         public string GetSystemTag()
         {
             return "";
+        }
+
+        public void StartProcess(string[] cmd)
+        {
+            System.Diagnostics.Process.Start(cmd[0], cmd.Skip(1).Aggregate("", (a, b) => a + " " + b));
         }
     }
 }
