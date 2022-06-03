bf6d8a4b58ff4a8ac32d2b9dfc77ebb7114eef8d
Michael Oborne <mich146@hotmail.com>
2022-06-03 11:07:48 +1000

    Xamarin: fix ssh bump

--- ExtLibs/Xamarin/GDAL/Resources/Resource.designer.cs
+++ ExtLibs/Xamarin/GDAL/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace GDAL
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.0.26")]
 	public partial class Resource
 	{
 		
