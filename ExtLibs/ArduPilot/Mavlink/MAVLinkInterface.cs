1c7c8a033e7bb3ecf22b797ae2062860ac922b6c
Randy Mackay <rmackay9@yahoo.com>
2022-06-23 15:15:20 +0900

    MAVLinkInterface: scale DO_GIMBAL_MANAGER_PITCHYAW

--- ExtLibs/ArduPilot/Mavlink/MAVLinkInterface.cs
+++ ExtLibs/ArduPilot/Mavlink/MAVLinkInterface.cs
@@ -3337,7 +3337,8 @@ Mission Planner waits for 2 valid heartbeat packets before connecting");
 
                         if (loc.id == (ushort) MAV_CMD.DO_DIGICAM_CONTROL ||
                             loc.id == (ushort) MAV_CMD.DO_DIGICAM_CONFIGURE ||
-                            loc.id == (ushort) MAV_CMD.ATTITUDE_TIME)
+                            loc.id == (ushort) MAV_CMD.ATTITUDE_TIME ||
+                            loc.id == (ushort) MAV_CMD.DO_GIMBAL_MANAGER_PITCHYAW)
                         {
                             loc.lat = wp.x;
                             loc.lng = wp.y;
@@ -3804,7 +3805,8 @@ Mission Planner waits for 2 valid heartbeat packets before connecting");
 
                 if (loc.id == (ushort) MAV_CMD.DO_DIGICAM_CONTROL || 
                     loc.id == (ushort) MAV_CMD.DO_DIGICAM_CONFIGURE || 
-                    loc.id == (ushort) MAV_CMD.ATTITUDE_TIME)
+                    loc.id == (ushort) MAV_CMD.ATTITUDE_TIME ||
+                    loc.id == (ushort) MAV_CMD.DO_GIMBAL_MANAGER_PITCHYAW)
                 {
                     req.y = (int) (loc.lng);
                     req.x = (int) (loc.lat);
