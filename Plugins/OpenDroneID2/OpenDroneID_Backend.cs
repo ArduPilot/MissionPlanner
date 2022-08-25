cb3c7be234c03fd91b3ffac3672a540d0fd01ba8
S. Borenstein <steve.borenstein@inspiredflight.com>
2022-08-24 22:37:14 -0600

    Fix for serial connected RID modules.

--- Plugins/OpenDroneID2/OpenDroneID_Backend.cs
+++ Plugins/OpenDroneID2/OpenDroneID_Backend.cs
@@ -18,8 +18,6 @@ namespace MissionPlanner
         private byte target_system;
         private byte target_component;
 
-        private byte target_broadcast = 0; 
-
         private  float rate_hz  = 0.1f;
         private float system_updaterate_hz  = 1.0f;
 
@@ -57,7 +55,7 @@ namespace MissionPlanner
         {
             _mav = mav;
             target_system = sysid;
-            target_component = compid;
+            target_component = (byte) MAVLink.MAV_COMPONENT.MAV_COMP_ID_ALL; // compid;
 
             running = true;
             timer = new Timer(Send, this, TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(100));
@@ -138,8 +136,8 @@ namespace MissionPlanner
         public void send_basic_id()
         {
             var basic_id =
-                MAVLink.mavlink_open_drone_id_basic_id_t.PopulateXMLOrder(target_broadcast,
-                    target_broadcast,
+                MAVLink.mavlink_open_drone_id_basic_id_t.PopulateXMLOrder(target_system,
+                    target_component,
                     id_or_mac(),
                     (byte)UAS_ID_type,
                     (byte)UA_type,
@@ -152,8 +150,8 @@ namespace MissionPlanner
         {
             // To meet compliance, sends 0's for lat/lng/alt if timeout
 
-            var id_system = MAVLink.mavlink_open_drone_id_system_t.PopulateXMLOrder(target_broadcast,
-                target_broadcast,
+            var id_system = MAVLink.mavlink_open_drone_id_system_t.PopulateXMLOrder(target_system,
+                target_component,
                 id_or_mac(),
                 (byte)operator_location_type,
                 (byte)classification_type,
@@ -167,29 +165,29 @@ namespace MissionPlanner
                 (byte)class_eu,
                 (since_last_msg_ms < _gps_timeout_ms) ? operator_altitude_geo:0.0f,
                 timestamp_2019());
-            _mav.sendPacket(id_system, target_broadcast, target_broadcast);
+            _mav.sendPacket(id_system, target_system, target_component);
         }
 
         public void send_system_update()
         {
             
-            var id_system = MAVLink.mavlink_open_drone_id_system_update_t.PopulateXMLOrder(target_broadcast,
-                target_broadcast,
+            var id_system = MAVLink.mavlink_open_drone_id_system_update_t.PopulateXMLOrder(target_system,
+                target_component,
                 (int)(operator_latitude * 1.0e7),
                 (int)(operator_longitude * 1.0e7),
                 operator_altitude_geo,
                 timestamp_2019());
-            _mav.sendPacket(id_system, target_broadcast, target_broadcast);
+            _mav.sendPacket(id_system, target_system, target_component);
         }
 
         public void send_self_id()
         {
-            var self_id = MAVLink.mavlink_open_drone_id_self_id_t.PopulateXMLOrder(target_broadcast,
-                target_broadcast,
+            var self_id = MAVLink.mavlink_open_drone_id_self_id_t.PopulateXMLOrder(target_system,
+                target_component,
                 id_or_mac(),
                 (byte)description_type,
                 to_string(description, 23));
-            _mav.sendPacket(self_id, target_broadcast, target_broadcast);
+            _mav.sendPacket(self_id, target_system, target_component);
         }
 
         private byte[] to_string(string s, int i)
@@ -199,12 +197,12 @@ namespace MissionPlanner
 
         public void send_operator_id()
         {
-            var operator_id_pkt = MAVLink.mavlink_open_drone_id_operator_id_t.PopulateXMLOrder(target_broadcast,
-                target_broadcast,
+            var operator_id_pkt = MAVLink.mavlink_open_drone_id_operator_id_t.PopulateXMLOrder(target_system,
+                target_component,
                 id_or_mac(),
                 (byte)operator_id_type,
                 to_string(operator_id, 20));
-            _mav.sendPacket(operator_id_pkt, target_broadcast, target_broadcast);
+            _mav.sendPacket(operator_id_pkt, target_system, target_component);
         }
 
         public byte[] id_or_mac()
