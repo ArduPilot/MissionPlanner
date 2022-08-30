ad0675601bcb3f035a1d11aa42aa833f2c5592c4
Michael Oborne <mich146@hotmail.com>
2022-08-31 09:50:01 +1000

    ConfigFirmwareManifest: add replug note

--- GCSViews/ConfigurationView/ConfigFirmwareManifest.cs
+++ GCSViews/ConfigurationView/ConfigFirmwareManifest.cs
@@ -338,7 +338,7 @@ namespace MissionPlanner.GCSViews.ConfigurationView
                 */
             }
 
-            CustomMessageBox.Show("Failed to detect port to upload to (Unknown VID/PID or Board String)", Strings.ERROR);
+            CustomMessageBox.Show("Failed to detect port to upload to (Unknown VID/PID or Board String)\r\nPlease try Disconnect/Reconnect and upload while on this screen", Strings.ERROR);
             return;
         }
 
