81e69c923bdc28a35946f35abab5265bbee571ce
Szilágyi Dániel <szilagyi.daniel88@gmail.com>
2022-10-04 10:56:45 +0200

    Fix: Form outside of display area (#2954)
    * ControlHelpers: restore form location inside display area
    
    * ControlHelpers: Code documentation
    
    ControlHelpers: Restore FormStartLocation position in file
    ControlHelpers: Missing line break

--- ExtLibs/Controls/ControlHelpers.cs
+++ ExtLibs/Controls/ControlHelpers.cs
@@ -11,6 +11,10 @@ namespace MissionPlanner.Controls
 {
     public static class ControlHelpers
     {
+        /// <summary>
+        /// Loads and applies saved form position and size settings from config
+        /// </summary>
+        /// <param name="control">Control to load and apply the settings for</param>
         public static void RestoreStartupLocation(this Form control)
         {
             var value = Settings.Instance[control.Text.Replace(" ", "_") + "_StartLocation"];
@@ -21,8 +25,8 @@ namespace MissionPlanner.Controls
                 {
                     var fsl = value.FromJSON<FormStartLocation>();
                     control.Location = fsl.Location;
-                    control.StartPosition = FormStartPosition.Manual;
                     control.Size = fsl.Size;
+                    control.StartPosition = RectVisible(control.Bounds) ? FormStartPosition.Manual : FormStartPosition.WindowsDefaultLocation;
                     control.WindowState = fsl.State;
                 } catch {}
             }
@@ -35,9 +39,27 @@ namespace MissionPlanner.Controls
             public FormWindowState State { get; set; }
         }
 
+        /// <summary>
+        /// Saves form position and size settings to config
+        /// </summary>
+        /// <param name="control">Control to save the settings for</param>
         public static void SaveStartupLocation(this Form control)
         {
             Settings.Instance[control.Text.Replace(" ", "_") + "_StartLocation"] = new FormStartLocation {Location = control.Location, Size = control.Size, State = control.WindowState}.ToJSON();
+        }
+
+        /// <summary>
+        /// Determines if a rectangle is visible on screen
+        /// </summary>
+        /// <param name="rectangle">Rectangle to check visibility for</param>
+        /// <returns>True, if the rectangle is fully visible, False, if even partially not</returns>
+        private static bool RectVisible(Rectangle rectangle)
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen.Bounds.Contains(rectangle)) return true;
+            }
+            return false;
         }
 
         public static void InvokeIfRequired<T>(this T control, Action<T> action) where T : ISynchronizeInvoke
