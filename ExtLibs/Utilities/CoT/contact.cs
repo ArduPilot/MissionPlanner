1709ae4e1adc8817660ccaf2590c65d61a39d4cb
Tom Pittenger <tom.pittenger@KrausAerospace.com>
2022-10-13 01:18:36 -0700

    CursorOnTarget: added advanced button with vmf and callsign endpoint

---
+++ ExtLibs/Utilities/CoT/contact.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace MissionPlanner.Utilities.CoT
+{
+    public class contact
+    {
+        [XmlAttribute] public string callsign;
+        [XmlAttribute] public string endpoint;
+    }
+}
