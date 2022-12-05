c8a9f5b18a923b5517a7d4841834bd53e16f6064
Michael Oborne <mich146@hotmail.com>
2022-12-05 14:34:22 +1100

    msi: installer update

--- Msi/installer.bat
+++ Msi/installer.bat
@@ -2,6 +2,8 @@
 
 for /f %%f in ('dir /a-d /b ..\bin\Release\net461\plugins') do if exist ..\bin\Release\net461\%%f del ..\bin\Release\net461\plugins\%%f
 
+powershell -command "cd..; ls plugins -recurse | ForEach-Object { if (Test-Path ($_.fullname -replace '\\plugins\\','\') -PathType Leaf) { $_.fullname }} | ForEach-Object { del $_ }"
+
 wix.exe ..\bin\release\net461\
 
 pause
