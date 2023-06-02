ea9c445a049ad46051acb7db0f0f93bd3acf4f1a
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-06-02 13:07:17 -0800

    OpenDroneID2: fixed swapped UA_Type and ID_Type

--- Plugins/OpenDroneID2/OpenDroneID_UI.cs
+++ Plugins/OpenDroneID2/OpenDroneID_UI.cs
@@ -270,10 +270,9 @@ namespace MissionPlanner.Controls
             if (_uas_id && CMB_uas_id_type.SelectedIndex > 0 && CMB_uas_type.SelectedIndex > 0)
             {
                 myDID.UAS_ID = TXT_UAS_ID.Text;
-                
-                myDID.UA_type = (MAVLink.MAV_ODID_ID_TYPE) CMB_uas_id_type.SelectedIndex;
-                myDID.UAS_ID_type = (MAVLink.MAV_ODID_UA_TYPE) CMB_uas_type.SelectedIndex;
 
+                myDID.UA_type = (MAVLink.MAV_ODID_UA_TYPE) CMB_uas_type.SelectedIndex;
+                myDID.UAS_ID_type = (MAVLink.MAV_ODID_ID_TYPE) CMB_uas_id_type.SelectedIndex;
             }
 
             if (TXT_self_id_TXT.Text.Length > 0)
