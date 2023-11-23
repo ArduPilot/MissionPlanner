a95b6b68b4a3f01c66e4586bf3cd1e792e3188f3
Bob Long <robertlong13@gmail.com>
2023-11-23 14:31:33 +1100

    Carbonix: add plugin

---
+++ plugins/Carbonix/Carbonix_Plugin.cs
@@ -0,0 +1,20 @@
+﻿using MissionPlanner.Plugin;
+
+namespace Carbonix
+{
+    public class CarbonixPlugin : Plugin
+    {
+        public override string Name { get; } = "Carbonix Addons";
+        public override string Version { get; } = "0.1";
+        public override string Author { get; } = "Carbonix";
+
+        public override bool Init() { return true; }
+
+        public override bool Loaded()
+        {
+            return true;
+        }
+
+        public override bool Exit() { return true; }
+    }
+}
