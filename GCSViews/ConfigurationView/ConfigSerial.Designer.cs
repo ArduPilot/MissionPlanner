77451af4a265b2da94b62eec429f500839e75df6
Andras Schaffer <eosbandi@gmail.com>
2023-11-28 21:45:29 +0100

    Setup: SerialPort settings screen (#3240)
    * Setup: SerialPort settings screen
    
    * Clean up usings to make OSX build happy
    
    * Moved down tab below Servo Output
    Add BRD_SERx_RTSCTS check
    Increase cell sizes to decrease clutter

---
+++ GCSViews/ConfigurationView/ConfigSerial.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MissionPlanner.GCSViews.ConfigurationView
+{
+    partial class ConfigSerial
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.myLabel1 = new MissionPlanner.Controls.MyLabel();
+            this.myLabel2 = new MissionPlanner.Controls.MyLabel();
+            this.myLabel3 = new MissionPlanner.Controls.MyLabel();
+            this.myLabel4 = new MissionPlanner.Controls.MyLabel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 152F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 114F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 132F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 307F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.myLabel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.myLabel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.myLabel3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.myLabel4, 3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 10);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 12;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(789, 497);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // myLabel1
+            // 
+            this.myLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.myLabel1.Location = new System.Drawing.Point(38, 9);
+            this.myLabel1.Name = "myLabel1";
+            this.myLabel1.resize = false;
+            this.myLabel1.Size = new System.Drawing.Size(75, 23);
+            this.myLabel1.TabIndex = 0;
+            this.myLabel1.Text = "Port Name";
+            // 
+            // myLabel2
+            // 
+            this.myLabel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.myLabel2.Location = new System.Drawing.Point(171, 9);
+            this.myLabel2.Name = "myLabel2";
+            this.myLabel2.resize = false;
+            this.myLabel2.Size = new System.Drawing.Size(75, 23);
+            this.myLabel2.TabIndex = 1;
+            this.myLabel2.Text = "Speed";
+            // 
+            // myLabel3
+            // 
+            this.myLabel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.myLabel3.Location = new System.Drawing.Point(294, 9);
+            this.myLabel3.Name = "myLabel3";
+            this.myLabel3.resize = false;
+            this.myLabel3.Size = new System.Drawing.Size(75, 23);
+            this.myLabel3.TabIndex = 2;
+            this.myLabel3.Text = "Protcol";
+            // 
+            // myLabel4
+            // 
+            this.myLabel4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.myLabel4.Location = new System.Drawing.Point(514, 9);
+            this.myLabel4.Name = "myLabel4";
+            this.myLabel4.resize = false;
+            this.myLabel4.Size = new System.Drawing.Size(75, 23);
+            this.myLabel4.TabIndex = 3;
+            this.myLabel4.Text = "Options";
+            // 
+            // ConfigSerial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ConfigSerial";
+            this.Size = new System.Drawing.Size(812, 519);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Controls.MyLabel myLabel1;
+        private Controls.MyLabel myLabel2;
+        private Controls.MyLabel myLabel3;
+        private Controls.MyLabel myLabel4;
+    }
+}
