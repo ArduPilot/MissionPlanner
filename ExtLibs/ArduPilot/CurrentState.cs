488c594a684e943d7a3de213ab23e5dacc4a7b40
Bob Long <robertlong13@gmail.com>
2023-12-16 18:37:29 +1100

    CurrentState: honor MAVLinkInterface.speechenable
    SW-18

--- ExtLibs/ArduPilot/CurrentState.cs
+++ ExtLibs/ArduPilot/CurrentState.cs
@@ -2634,6 +2634,7 @@ namespace MissionPlanner
                                 try
                                 {
                                     if (oldmode != mode && Speech != null && Speech.speechEnable &&
+                                        parent?.parent?.speechenabled == true &&
                                         parent?.parent?.MAV?.cs == this &&
                                         Settings.Instance.GetBoolean("speechmodeenabled") &&
                                         (armed || !Settings.Instance.GetBoolean("speech_armed_only")))
@@ -3131,6 +3132,7 @@ namespace MissionPlanner
                             try
                             {
                                 if (oldwp != wpno && Speech != null && Speech.speechEnable && parent != null &&
+                                    parent.parent.speechenabled &&
                                     parent.parent.MAV.cs == this &&
                                     Settings.Instance.GetBoolean("speechwaypointenabled") &&
                                     (armed || !Settings.Instance.GetBoolean("speech_armed_only")))
