0bb3a2d5922de287ff1345984852c01b79ab1c0b
Bob Long <robertlong13@gmail.com>
2024-08-15 11:04:54 +1000

    MAVLinkParam: fix rounding to 7 digits (#3389)
    Rounding to fixed number of decimal places led to over rounding small
    numbers and under-rounding big numbers.

--- ExtLibs/Mavlink/MAVLinkParam.cs
+++ ExtLibs/Mavlink/MAVLinkParam.cs
@@ -129,10 +129,19 @@ item.float_value
 (double)item.float_value
 0.800000011920929
  */
-                    return Math.Round((double)float_value, 7);
+                    return RoundToSignificantDigits(float_value, 7);
             }
 
             throw new FormatException("invalid type");
+        }
+
+        private double RoundToSignificantDigits(double d, int digits)
+        {
+            if (d == 0)
+                return 0;
+
+            double scale = Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(d))) + 1);
+            return scale * Math.Round(d / scale, digits);
         }
 
         public void SetValue(double input)
