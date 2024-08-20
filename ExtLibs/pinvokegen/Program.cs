9315a8d505c822a81cae2a8fc7248a9c058c73c2
Michael Oborne <mich146@hotmail.com>
2024-08-20 11:40:20 +1000

    pinkvokegen

---
+++ ExtLibs/pinvokegen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace pinvokegen
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Windows.Win32.PInvoke.FindWindow("Notepad", null);
+            Windows.Win32.PInvoke.GetConsoleWindow();
+        }
+    }
+}
