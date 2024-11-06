fb0c00ccfbebf09a4567273ec5b5832ef27971bb
Michael Oborne <mich146@hotmail.com>
2024-11-06 13:10:32 +1100

    SerialOutputNMEA: always output alt as M

--- Controls/SerialOutputNMEA.cs
+++ Controls/SerialOutputNMEA.cs
@@ -144,7 +144,7 @@ namespace MissionPlanner.Controls
                         DateTime.Now.ToUniversalTime(), Math.Abs(lat * 100).ToString("0000.00000", CultureInfo.InvariantCulture), MainV2.comPort.MAV.cs.lat < 0 ? "S" : "N",
                         Math.Abs(lng * 100).ToString("00000.00000", CultureInfo.InvariantCulture), MainV2.comPort.MAV.cs.lng < 0 ? "W" : "E",
                         MainV2.comPort.MAV.cs.gpsstatus >= 3 ? 1 : 0, MainV2.comPort.MAV.cs.satcount,
-                        MainV2.comPort.MAV.cs.gpshdop, MainV2.comPort.MAV.cs.altasl, "M", 0, "M", "");
+                        MainV2.comPort.MAV.cs.gpshdop, MainV2.comPort.MAV.cs.altasl / CurrentState.multiplieralt, "M", 0, "M", "");
 
                     string checksum = GetChecksum(line);
                     NmeaStream.WriteLine(line + "*" + checksum+"\r");
