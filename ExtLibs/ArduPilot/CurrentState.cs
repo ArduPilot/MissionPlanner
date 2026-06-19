48327200c67bd1cf265ece8a4b94fc14c0289a15
Michael Oborne <michael@cubepilot.com>
2026-06-19 15:14:39 +1000

    CurrentState: hide simstate

--- ExtLibs/ArduPilot/CurrentState.cs
+++ ExtLibs/ArduPilot/CurrentState.cs
@@ -2290,17 +2290,17 @@ namespace MissionPlanner
                     case (uint)MAVLink.MAVLINK_MSG_ID.SIMSTATE:
                         {
                             var simstate = mavLinkMessage.ToStructure<MAVLink.mavlink_simstate_t>();
-                            lat = simstate.lat / 1e7;
-                            lng = simstate.lng / 1e7;
-                            roll = (float)(simstate.roll * MathHelper.rad2deg);
-                            pitch = (float)(simstate.pitch * MathHelper.rad2deg);
-                            yaw = (float)(simstate.yaw * MathHelper.rad2deg);
-                            gy = simstate.ygyro;
-                            gx = simstate.xgyro;
-                            gz = simstate.zgyro;
-                            ax = simstate.xacc;
-                            ay = simstate.yacc;
-                            az = simstate.zacc;
+                            //lat = simstate.lat / 1e7;
+                            //lng = simstate.lng / 1e7;
+                            //roll = (float)(simstate.roll * MathHelper.rad2deg);
+                            //pitch = (float)(simstate.pitch * MathHelper.rad2deg);
+                            //yaw = (float)(simstate.yaw * MathHelper.rad2deg);
+                            //gy = simstate.ygyro;
+                            //gx = simstate.xgyro;
+                            //gz = simstate.zgyro;
+                            //ax = simstate.xacc;
+                            //ay = simstate.yacc;
+                            //az = simstate.zacc;
                         }
                         break;
 
